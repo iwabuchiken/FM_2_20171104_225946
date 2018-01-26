dde5e92dabad7c04457f3ce5da255b707f92db38
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-01-26 23:31:45 +0900

    W

--- Materials_Science/Materials_Science.mm
+++ Materials_Science/Materials_Science.mm
@@ -1,59 +1,40 @@
 <map version="1.0.1">
-
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516530682576" ID="ID_1289426702" MODIFIED="1516687082825" TEXT="Materials Science">
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516530834781" ID="ID_625798212" MODIFIED="1516687163716" POSITION="right" STYLE="bubble" TEXT="1) Materials">
-<cloud COLOR="#ff9999" />
-<icon BUILTIN="full-1" />
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516530839900" ID="ID_1801229288" LINK="http://benfranklin.chips.jp/cake_apps/CR6_cake/texts/view/2223" MODIFIED="1516687084077" TEXT="1-1) Graphene">
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516530845277" ID="ID_1944155176" MODIFIED="1516687084077" TEXT="1-1-1)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 19:34:05.277000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 19:33:59.900000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
+<cloud COLOR="#ff9999"/>
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1516530839900" ID="ID_1801229288" LINK="http://benfranklin.chips.jp/cake_apps/CR6_cake/texts/view/2223" MODIFIED="1516687084077" TEXT="1-1) Graphene"/>
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516536392563" ID="ID_71095160" LINK="https://www.materialstoday.com/nanomaterials/news/novel-facets-of-colloidal-metal-nanocrystals/" MODIFIED="1516687084077" TEXT="1-2) nanocrystals">
-<font NAME="SansSerif" SIZE="12" />
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536405364" ID="ID_667071178" MODIFIED="1516687084077" TEXT="1-2-1)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 21:06:45.364000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516537022467" ID="ID_501942205" LINK="https://www.materialstoday.com/nanomaterials/news/novel-facets-of-colloidal-metal-nanocrystals/" MODIFIED="1516687084077" TEXT="1-2-2) nanocrystal seeds">
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536405364" ID="ID_355946107" MODIFIED="1516687084077" TEXT="1-2-2-1)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 21:06:45.364000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 21:17:02.467000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
+<font NAME="SansSerif" SIZE="12"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1516537022467" ID="ID_501942205" LINK="https://www.materialstoday.com/nanomaterials/news/novel-facets-of-colloidal-metal-nanocrystals/" MODIFIED="1516687084077" TEXT="1-2-2) nanocrystal seeds"/>
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516537071171" ID="ID_97277297" LINK="https://www.materialstoday.com/nanomaterials/news/novel-facets-of-colloidal-metal-nanocrystals/" MODIFIED="1516687084077" TEXT="1-2-3)  nanocrystal facets">
-<arrowlink DESTINATION="ID_67216640" ENDARROW="Default" ENDINCLINATION="29;0;" ID="Arrow_ID_1960638127" STARTARROW="None" STARTINCLINATION="29;0;" />
-<attribute NAME="created" VALUE="2018-01-21 21:17:51.171000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
+<arrowlink DESTINATION="ID_67216640" ENDARROW="Default" ENDINCLINATION="29;0;" ID="Arrow_ID_1960638127" STARTARROW="None" STARTINCLINATION="29;0;"/>
+</node>
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516537085474" ID="ID_67216640" MODIFIED="1516687084077" TEXT="1-2-4) surface structures">
-<linktarget COLOR="#b0b0b0" DESTINATION="ID_67216640" ENDARROW="Default" ENDINCLINATION="29;0;" ID="Arrow_ID_1960638127" SOURCE="ID_97277297" STARTARROW="None" STARTINCLINATION="29;0;" />
-<attribute NAME="created" VALUE="2018-01-21 21:18:05.474000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 21:06:32.563000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536945244" ID="ID_1101678067" MODIFIED="1516687084077" TEXT="1-3)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 21:15:45.244000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 19:33:54.781000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_67216640" ENDARROW="Default" ENDINCLINATION="29;0;" ID="Arrow_ID_1960638127" SOURCE="ID_97277297" STARTARROW="None" STARTINCLINATION="29;0;"/>
+</node>
+</node>
+<node CREATED="1516802080155" ID="ID_1776269575" MODIFIED="1516802086711" TEXT="Plastic">
+<node CREATED="1516802088731" ID="ID_296652321" MODIFIED="1516802103262" TEXT="&#x4ee3;&#x66ff;&#x7269;&#x8cea;">
+<node CREATED="1516802109625" ID="ID_1867725154" MODIFIED="1516802115695" TEXT="&#x8a18;&#x4e8b;">
+<node CREATED="1516802117138" ID="ID_1924328901" LINK="https://duckduckgo.com/?q=%E3%83%97%E3%83%A9%E3%82%B9%E3%83%81%E3%83%83%E3%82%AF+%E4%BB%A3%E6%9B%BF&amp;atb=v84-1__&amp;ia=web" MODIFIED="1516802125125" TEXT="search"/>
+</node>
+<node CREATED="1516839052928" ID="ID_1670146038" MODIFIED="1516839059555" TEXT="&#x88fd;&#x54c1;"/>
+</node>
+</node>
+</node>
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516536909468" ID="ID_47738979" MODIFIED="1516687226679" POSITION="right" TEXT="2) Applications">
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536919611" ID="ID_776380937" MODIFIED="1516687226679" TEXT="2-1) catalytic">
-<attribute NAME="created" VALUE="2018-01-21 21:15:19.611000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536922763" ID="ID_648692082" MODIFIED="1516687226679" TEXT="2-2) biomedical">
-<attribute NAME="created" VALUE="2018-01-21 21:15:22.763000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536930251" ID="ID_292921180" MODIFIED="1516687226679" TEXT="2-3) photonic">
-<attribute NAME="created" VALUE="2018-01-21 21:15:30.251000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536935411" ID="ID_1694055485" MODIFIED="1516687226679" TEXT="2-4) electronic">
-<attribute NAME="created" VALUE="2018-01-21 21:15:35.411000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536945244" ID="ID_948315052" MODIFIED="1516687226679" TEXT="2-5)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 21:15:45.244000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 21:15:09.468000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536919611" ID="ID_776380937" MODIFIED="1516687226679" TEXT="2-1) catalytic"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536922763" ID="ID_648692082" MODIFIED="1516687226679" TEXT="2-2) biomedical"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536930251" ID="ID_292921180" MODIFIED="1516687226679" TEXT="2-3) photonic"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536935411" ID="ID_1694055485" MODIFIED="1516687226679" TEXT="2-4) electronic"/>
+</node>
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516536987948" ID="ID_1563805381" MODIFIED="1516687200084" POSITION="right" TEXT="3) Substances">
-<cloud />
+<cloud/>
 <node BACKGROUND_COLOR="#ccffcc" CREATED="1516536992019" ID="ID_57884051" LINK="https://www.materialstoday.com/nanomaterials/news/novel-facets-of-colloidal-metal-nanocrystals/" MODIFIED="1516687248825" TEXT="3-1) precursor chemicals">
-<cloud COLOR="#33ffff" />
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536945244" ID="ID_1531582882" MODIFIED="1516687231228" TEXT="3-1-1)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 21:15:45.244000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 21:16:32.019000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<node BACKGROUND_COLOR="#ccffcc" CREATED="1516536945244" ID="ID_1999634601" MODIFIED="1516687082825" TEXT="3-2)  18/01/21">
-<icon BUILTIN="calendar" />
-<attribute NAME="created" VALUE="2018-01-21 21:15:45.244000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 21:16:27.948000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
-<attribute NAME="created" VALUE="2018-01-21 19:31:22.576000" /><attribute_layout NAME_WIDTH="75" VALUE_WIDTH="120" /></node>
+<cloud COLOR="#33ffff"/>
+</node>
+</node>
+</node>
 </map>
