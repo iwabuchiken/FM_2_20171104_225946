61cc863ad917af0cc8c6dd3dd503d4e9aef76693
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-02-10 23:28:41 +0900

    W

--- Projects/5 geisya.mm
+++ Projects/5 geisya.mm
@@ -1,10 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1516760159782" ID="ID_360943075" LINK="Projects.mm" MODIFIED="1517456465532" TEXT="5 geisya">
-<node CREATED="1517036659582" ID="ID_24459744" MODIFIED="1517802883128" POSITION="right" TEXT="1) NEXT*">
-<cloud COLOR="#66ff99"/>
-<node CREATED="1517036738602" ID="ID_1331973387" MODIFIED="1517036738602" TEXT="1-1)  18/01/27"/>
-</node>
 <node CREATED="1517455868946" ID="ID_296511950" MODIFIED="1517455872588" POSITION="right" TEXT="2) &#x884c;&#x5217;">
 <node CREATED="1517455903166" ID="ID_966568424" LINK="http://www.geisya.or.jp/~mwm48961/kou3/mobile/matrix_power_m.htm" MODIFIED="1517455907676" TEXT="2-1) &#x884c;&#x5217;&#x306e;&#xff4e;&#x4e57;(3)">
 <node CREATED="1517455919920" ID="ID_783771693" LINK="http://www.geisya.or.jp/~mwm48961/kou3/mobile/matrix_power_m.htm#three" MODIFIED="1517456030177" TEXT="2-1-1) [3]&#x3000;&#x6570;&#x5217;&#x306e;&#x9023;&#x7acb;&#x6f38;&#x5316;&#x5f0f;&#x306b;&#x3057;&#x3066;&#x89e3;&#x304f;&#x65b9;&#x6cd5;">
