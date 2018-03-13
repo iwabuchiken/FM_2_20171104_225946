f7cceffa48c58c83e624a83d6988661d680c86ed
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-03-13 22:26:34 +0900

    update :
    ---
    
    # On branch master
    # Changes to be committed:
    #	modified:   "Projects/11 \343\203\231\343\202\257\343\203\210\343\203\253\343\200\201\343\203\206\343\203\263\343\202\275\343\203\253.mm"
    #	modified:   Projects/Projects.mm

--- Projects/11 ベクトル、テンソル.mm
+++ Projects/11 ベクトル、テンソル.mm
@@ -9,7 +9,11 @@
 <node CREATED="1519742510329" ID="ID_1244207174" MODIFIED="1519856711938" TEXT="http://benfranklin.chips.jp/cake_apps/images/mindmaps"/>
 </node>
 </node>
-<node CREATED="1520395885659" ID="ID_1274831480" LINK="http://hooktail.sub.jp/vectoranalysis/MetricTensor/" MODIFIED="1520395893626" POSITION="right" TEXT="&#x8a08;&#x91cf;&#x30c6;&#x30f3;&#x30bd;&#x30eb;"/>
+<node CREATED="1520904733800" ID="ID_905088586" LINK="http://hooktail.sub.jp/vectoranalysis/CovariantContravariant/" MODIFIED="1520904763876" POSITION="right" TEXT="&#x5171;&#x5909;&#x30d9;&#x30af;&#x30c8;&#x30eb;&#x3000;&#x53cd;&#x5909;&#x30d9;&#x30af;&#x30c8;&#x30eb;">
+<node CREATED="1520904696992" ID="ID_1374025943" LINK="http://hooktail.sub.jp/vectoranalysis/DualBases/" MODIFIED="1520904718642" TEXT="&#x53cc;&#x5bfe;&#x57fa;&#x5e95;">
+<node CREATED="1520395885659" ID="ID_1274831480" LINK="http://hooktail.sub.jp/vectoranalysis/MetricTensor/" MODIFIED="1520395893626" TEXT="&#x8a08;&#x91cf;&#x30c6;&#x30f3;&#x30bd;&#x30eb;"/>
+</node>
+</node>
 <node CREATED="1519740246386" ID="ID_1234471347" MODIFIED="1519856598212" POSITION="right" TEXT="HISTORY"/>
 </node>
 </map>
