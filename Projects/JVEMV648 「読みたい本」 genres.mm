cf6c5ebbf59f708823ea40f25eacc7f3e077a6bf
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-03-22 18:09:22 +0900

    update :
    ---
    g c -a
    
     Changes to be committed:
    	modified:   Projects/46 Art.mm
    	modified:   Projects/JVEMV6.mm
    	new file:   "Projects/JVEMV648 \343\200\214\350\252\255\343\201\277\343\201\237\343\201\204\346\234\254\343\200\215 genres.mm"
    	new file:   "Projects/JVEMV6_48 \343\200\214\350\252\255\343\201\277\343\201\237\343\201\204\346\234\254\343\200\215 genres.mm"
    	modified:   Projects/Projects.mm

---
+++ Projects/JVEMV648 「読みたい本」 genres.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1521580264139" ID="ID_1099598887" LINK="JVEMV6.mm" MODIFIED="1521580310282" TEXT="48 &#x300c;&#x8aad;&#x307f;&#x305f;&#x3044;&#x672c;&#x300d; genres"/>
+</map>
