8d6ec53b1ab492952f9bf028c91d2192041b8c4e
iwabuchiken <iwabuchi.k.2010@gmail.com>
2018-04-03 11:03:45 +0900

    update :
    ---
    g c -a
    g2 c -a
    
    # Changes to be committed:
    #	modified:   Projects/46 Art.5 clarinet.mm
    #	modified:   Projects/46 Art.mm
    #	new file:   Projects/46 Art_7 Rythms.mm

---
+++ Projects/46 Art_7 Rythms.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1522707180374" ID="ID_174219456" LINK="46%20Art.mm" MODIFIED="1522707208092" TEXT="7 Rythms"/>
+</map>
