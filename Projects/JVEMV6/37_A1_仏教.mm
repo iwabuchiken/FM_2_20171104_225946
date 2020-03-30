ce0d66e489bab17618e6279c5801c13b2033375d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-03-30 14:08:29 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/37_A1_仏教.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1577864374142" ID="ID_874024617" LINK="../JVEMV6.mm" MODIFIED="1577864425280" TEXT="A1 &#x4ecf;&#x6559;">
+<node CREATED="1577864444064" ID="ID_1527608836" LINK="https://ja.wikipedia.org/wiki/%E6%96%87%E6%AE%8A%E8%8F%A9%E8%96%A9" MODIFIED="1577864450469" POSITION="right" TEXT="&#x6587;&#x6b8a;&#x83e9;&#x85a9;"/>
+</node>
+</map>
