93f37ef81273b64b159621ae982008cb4ba8caf8
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-07-19 16:19:29 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/37_miscs/21_math-physics/4_knot-theory.mm
+++ Projects/JVEMV6/37_miscs/21_math-physics/4_knot-theory.mm
@@ -1,4 +1,40 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1592544148673" ID="ID_1436701445" LINK="../../../JVEMV6.mm" MODIFIED="1595138592303" TEXT="4. knot theory"/>
+<node CREATED="1592544148673" ID="ID_1436701445" LINK="../../../JVEMV6.mm" MODIFIED="1595138592303" TEXT="4. knot theory">
+<node CREATED="1595140161299" ID="ID_1902141822" MODIFIED="1595140164435" POSITION="right" TEXT="THEOREM">
+<node CREATED="1595141142672" ID="ID_709506569" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42336" MODIFIED="1595141144638" TEXT="1">
+<node CREATED="1595141150120" ID="ID_1260118676" MODIFIED="1595141551480" TEXT="&#x96a3;&#x308a;&#x5408;&#x3046;&#xff12;&#x3064;&#x306e; cross &#x304c; dissolve &#xb420; &#xc870;&#xac74;"/>
+</node>
+<node CREATED="1595140638243" ID="ID_237696531" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42396" MODIFIED="1595140640528" TEXT="2">
+<node CREATED="1595140679515" ID="ID_411420906" MODIFIED="1595141547807" TEXT="cross &#x304c; dissolve &#xb420; &#xc870;&#xac74;"/>
+</node>
+<node CREATED="1595140222567" ID="ID_626099594" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42395" MODIFIED="1595140225431" TEXT="3">
+<node CREATED="1595140705954" ID="ID_1779320913" MODIFIED="1595141687299" TEXT="cross &#xd55c; &#xac1c;&#xc5d0;&#xc11c; &#x3001;knot &#x3092;&#xb9cc;&#xb4e4;&#xae30;&#xc704;&#xd55c; &#xd544;&#xc694;&#xc870;&#xac74;"/>
+<node CREATED="1595141201649" ID="ID_1374681015" MODIFIED="1595141211416" TEXT="&#x5341;&#x5206;&#x6761;&#x4ef6;&#x304b;&#xff1f;&#x3000;=&gt;&#x3000;&#x672a;&#x691c;&#x8a0e;"/>
+</node>
+<node CREATED="1595141825636" ID="ID_950924394" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42043" MODIFIED="1595141832507" TEXT="4">
+<node CREATED="1595141876691" ID="ID_1790257715" MODIFIED="1595141912045" TEXT="THEO-1 &#xacfc; &#xb3d9;&#xc77c;"/>
+</node>
+<node CREATED="1595141423077" ID="ID_1956395970" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42043" MODIFIED="1595141965235" TEXT="5">
+<node CREATED="1595141478980" ID="ID_59439854" MODIFIED="1595142001159" TEXT="&quot;through&quot; &#xc640; &quot;cut&quot; &#xb97c; &#xd560; &#xacbd;&#xc6b0;&#xc758; &#xd6a8;&#xacfc;"/>
+</node>
+<node CREATED="1595141969197" ID="ID_1403984367" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42065" MODIFIED="1595141973468" TEXT="6">
+<node CREATED="1595141876691" ID="ID_1047636598" MODIFIED="1595142079423" TEXT="THEO-5 &#xacfc; &#xb3d9;&#xc77c;">
+<node CREATED="1595142079805" ID="ID_363810042" MODIFIED="1595142079805" TEXT="==&gt; &#xb354; &#xae4a;&#xac8c;"/>
+</node>
+</node>
+<node CREATED="1595142924105" ID="ID_581611084" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42348" MODIFIED="1595142926252" TEXT="7">
+<node CREATED="1595142927381" ID="ID_645611739" MODIFIED="1595142929364" TEXT="loop">
+<node CREATED="1595142930214" ID="ID_547978664" MODIFIED="1595142948536" TEXT="turn closed loop into ==&gt; opened one"/>
+</node>
+</node>
+</node>
+<node CREATED="1595142906187" ID="ID_1785685844" MODIFIED="1595142907560" POSITION="right" TEXT="DEF">
+<node CREATED="1595142908816" ID="ID_1440232490" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42348" MODIFIED="1595142912511" TEXT="1">
+<node CREATED="1595142913671" ID="ID_137195494" MODIFIED="1595142915654" TEXT="loop"/>
+</node>
+</node>
+<node CREATED="1595142969998" ID="ID_1653552084" MODIFIED="1595142971809" POSITION="right" TEXT="WORD"/>
+<node CREATED="1595138673241" ID="ID_1012298114" LINK="../../../../../../Utils/commands/push_fm.bat" MODIFIED="1595138725590" POSITION="left" TEXT="git"/>
+</node>
 </map>
