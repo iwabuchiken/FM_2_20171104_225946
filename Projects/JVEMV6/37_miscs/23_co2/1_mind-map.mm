c69d3f5a323ca80fe5bcfcc297e2846664b6f93a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-08-30 18:25:14 +0900

    [10] res free# JVEMV6 37#23.1_X / 37. miscs / 23. CO2 / 1. mind-map / 20200830_175318

--- Projects/JVEMV6/37_miscs/23_co2/1_mind-map.mm
+++ Projects/JVEMV6/37_miscs/23_co2/1_mind-map.mm
@@ -1,55 +1,55 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1593940140603" ID="ID_973698391" LINK="../../../JVEMV6.mm" MODIFIED="1593940315930" TEXT="1 mind map">
-<node CREATED="1595143381339" FOLDED="true" ID="ID_16857522" LINK="https://pubs.rsc.org/en/content/articlelanding/2017/cs/c6cs00066e#!divAbstract" MODIFIED="1598605096596" POSITION="right" TEXT="M-47">
-<node CREATED="1595143417706" ID="ID_319301539" MODIFIED="1595143418349" TEXT="#23.4">
-<node CREATED="1595143384309" ID="ID_868947392" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42513" MODIFIED="1595143679836" TEXT="&quot;general challenge&quot;">
-<node CREATED="1595143514121" ID="ID_1741634753" MODIFIED="1595143516479" TEXT="research">
-<node CREATED="1595143473100" ID="ID_989480409" MODIFIED="1595143473945" TEXT="1">
-<node CREATED="1595143475620" ID="ID_524482394" MODIFIED="1595143479944" TEXT="plasma-chemical process">
+<node CREATED="1595143381339" FOLDED="true" ID="ID_16857522" LINK="https://pubs.rsc.org/en/content/articlelanding/2017/cs/c6cs00066e#!divAbstract" MODIFIED="1598777701475" POSITION="right" TEXT="M-47">
+<node CREATED="1595143417706" FOLDED="true" ID="ID_319301539" MODIFIED="1598777701459" TEXT="#23.4">
+<node CREATED="1595143384309" FOLDED="true" ID="ID_868947392" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42513" MODIFIED="1598777701459" TEXT="&quot;general challenge&quot;">
+<node CREATED="1595143514121" FOLDED="true" ID="ID_1741634753" MODIFIED="1598777701459" TEXT="research">
+<node CREATED="1595143473100" FOLDED="true" ID="ID_989480409" MODIFIED="1598777701444" TEXT="1">
+<node CREATED="1595143475620" FOLDED="true" ID="ID_524482394" MODIFIED="1598777701444" TEXT="plasma-chemical process">
 <node CREATED="1595143482992" ID="ID_8933431" MODIFIED="1595143488586" TEXT="MW"/>
 <node CREATED="1595143488857" ID="ID_1801309847" MODIFIED="1595143489686" TEXT="GA"/>
 </node>
 </node>
-<node CREATED="1595143493373" ID="ID_1211654290" MODIFIED="1595143493765" TEXT="2">
-<node CREATED="1595143498202" ID="ID_34870142" MODIFIED="1595143502868" TEXT="plasma + catalysis">
-<node CREATED="1595143542708" ID="ID_60885269" MODIFIED="1595143548077" TEXT="1">
+<node CREATED="1595143493373" FOLDED="true" ID="ID_1211654290" MODIFIED="1598777701444" TEXT="2">
+<node CREATED="1595143498202" FOLDED="true" ID="ID_34870142" MODIFIED="1598777701444" TEXT="plasma + catalysis">
+<node CREATED="1595143542708" FOLDED="true" ID="ID_60885269" MODIFIED="1598777701444" TEXT="1">
 <node CREATED="1595143550937" ID="ID_694627042" MODIFIED="1595143557897" TEXT="synergy effect"/>
 </node>
-<node CREATED="1595143559292" ID="ID_1247062955" MODIFIED="1595143560673" TEXT="2">
+<node CREATED="1595143559292" FOLDED="true" ID="ID_1247062955" MODIFIED="1598777701444" TEXT="2">
 <node CREATED="1595143560674" ID="ID_1326891290" MODIFIED="1595143566163" TEXT="selection"/>
 </node>
 </node>
 </node>
-<node CREATED="1595143583831" ID="ID_1960349560" MODIFIED="1595143585492" TEXT="3">
-<node CREATED="1595143585493" ID="ID_90507238" MODIFIED="1595143590523" TEXT="O/O2">
-<node CREATED="1595143590524" ID="ID_1840560505" MODIFIED="1595143592303" TEXT="trapping">
-<node CREATED="1595143593213" ID="ID_392215499" MODIFIED="1595143594697" TEXT="in situ">
-<node CREATED="1595143600007" ID="ID_1557323638" MODIFIED="1595143618668" TEXT="1. by">
+<node CREATED="1595143583831" FOLDED="true" ID="ID_1960349560" MODIFIED="1598777701459" TEXT="3">
+<node CREATED="1595143585493" FOLDED="true" ID="ID_90507238" MODIFIED="1598777701459" TEXT="O/O2">
+<node CREATED="1595143590524" FOLDED="true" ID="ID_1840560505" MODIFIED="1598777701444" TEXT="trapping">
+<node CREATED="1595143593213" FOLDED="true" ID="ID_392215499" MODIFIED="1598777701444" TEXT="in situ">
+<node CREATED="1595143600007" FOLDED="true" ID="ID_1557323638" MODIFIED="1598777701444" TEXT="1. by">
 <node CREATED="1595143602575" ID="ID_866963264" MODIFIED="1595143604558" TEXT="scavenger"/>
 </node>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1595323414652" ID="ID_1838466758" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537" MODIFIED="1595323419401" TEXT="4">
+<node CREATED="1595323414652" FOLDED="true" ID="ID_1838466758" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537" MODIFIED="1598777701459" TEXT="4">
 <node CREATED="1595323442471" ID="ID_479557129" MODIFIED="1595323444460" TEXT="scale-up"/>
 </node>
-<node CREATED="1595323420390" ID="ID_1252341123" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537" MODIFIED="1595323424490" TEXT="5">
+<node CREATED="1595323420390" FOLDED="true" ID="ID_1252341123" LINK="http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537" MODIFIED="1598777701459" TEXT="5">
 <node CREATED="1595323448142" ID="ID_946131438" MODIFIED="1595323451979" TEXT="power supplies"/>
 </node>
 </node>
 </node>
 </node>
-<node CREATED="1595143417706" ID="ID_506726976" MODIFIED="1595323533842" TEXT="#23.5">
-<node CREATED="1595323538775" ID="ID_1196553400" LINK="&quot;general challenge&quot; &lt;http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42513&gt;     research         1             plasma-chemical process                 MW                 GA         2             plasma + catalysis                 1                     synergy effect                 2                     selection         3             O/O2                 trapping                     in situ                         1. by                             scavenger         4 &lt;http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537&gt;             scale-up         5 &lt;http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537&gt;             power supplies " MODIFIED="1595323550017" TEXT="discharge-specific challen.">
+<node CREATED="1595143417706" FOLDED="true" ID="ID_506726976" MODIFIED="1598777701475" TEXT="#23.5">
+<node CREATED="1595323538775" FOLDED="true" ID="ID_1196553400" LINK="&quot;general challenge&quot; &lt;http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42513&gt;     research         1             plasma-chemical process                 MW                 GA         2             plasma + catalysis                 1                     synergy effect                 2                     selection         3             O/O2                 trapping                     in situ                         1. by                             scavenger         4 &lt;http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537&gt;             scale-up         5 &lt;http://benfranklin.chips.jp/cake_apps/Cake_IFM11/images/view/42537&gt;             power supplies " MODIFIED="1598777701459" TEXT="discharge-specific challen.">
 <node CREATED="1595323556371" ID="ID_1687351001" MODIFIED="1595323558003" TEXT="MW"/>
 <node CREATED="1595323559853" ID="ID_529797578" MODIFIED="1595323561308" TEXT="DRM"/>
 <node CREATED="1595323568638" ID="ID_819634200" MODIFIED="1595323569267" TEXT="GA"/>
 <node CREATED="1595323573287" ID="ID_1698763359" MODIFIED="1595323629309" TEXT="other reactors"/>
 </node>
 </node>
-<node CREATED="1595833708172" ID="ID_62576267" MODIFIED="1595835489889" TEXT="conv with co-reactant">
+<node CREATED="1595833708172" FOLDED="true" ID="ID_62576267" MODIFIED="1598777701475" TEXT="conv with co-reactant">
 <node CREATED="1595833867276" ID="ID_557339905" MODIFIED="1595833872465" TEXT="sec : 2.2.1"/>
 <node CREATED="1595833734371" FOLDED="true" ID="ID_36792036" MODIFIED="1595835202073" TEXT="1">
 <node CREATED="1595833738154" ID="ID_50347478" MODIFIED="1595833739653" TEXT="CH4"/>
@@ -95,23 +95,23 @@
 </node>
 </node>
 </node>
-<node CREATED="1595835216399" ID="ID_1546236507" MODIFIED="1595835502307" TEXT="novel approaches">
-<node CREATED="1595835222382" ID="ID_1224804227" MODIFIED="1595835224537" TEXT="sec: 3">
+<node CREATED="1595835216399" FOLDED="true" ID="ID_1546236507" MODIFIED="1598777701475" TEXT="novel approaches">
+<node CREATED="1595835222382" FOLDED="true" ID="ID_1224804227" MODIFIED="1598777701475" TEXT="sec: 3">
 <node CREATED="1595835506728" ID="ID_318863199" MODIFIED="1595835509889" TEXT="#9"/>
 </node>
-<node CREATED="1595835377269" ID="ID_1533504916" MODIFIED="1597497889829" TEXT="1">
+<node CREATED="1595835377269" FOLDED="true" ID="ID_1533504916" MODIFIED="1598777701475" TEXT="1">
 <node CREATED="1595835380990" ID="ID_829033884" MODIFIED="1595835406264" TEXT="Electro-chemical conv"/>
 </node>
-<node CREATED="1595835378197" ID="ID_1194473291" MODIFIED="1597497889845" TEXT="2">
+<node CREATED="1595835378197" FOLDED="true" ID="ID_1194473291" MODIFIED="1598777701475" TEXT="2">
 <node CREATED="1595835380990" ID="ID_791991119" MODIFIED="1595835402497" TEXT="solar thermo-chemical conv"/>
 </node>
-<node CREATED="1595835379219" ID="ID_1460464833" MODIFIED="1597497889845" TEXT="3">
+<node CREATED="1595835379219" FOLDED="true" ID="ID_1460464833" MODIFIED="1598777701475" TEXT="3">
 <node CREATED="1595835418555" ID="ID_696443483" MODIFIED="1595835423830" TEXT="photo-chemic. conv"/>
 </node>
-<node CREATED="1595835434717" ID="ID_1252414930" MODIFIED="1597497889845" TEXT="4">
+<node CREATED="1595835434717" FOLDED="true" ID="ID_1252414930" MODIFIED="1598777701475" TEXT="4">
 <node CREATED="1595835438258" ID="ID_1393070253" MODIFIED="1595835443096" TEXT="bio-chem. conv"/>
 </node>
-<node CREATED="1595835435786" ID="ID_1381917212" MODIFIED="1597497889845" TEXT="5">
+<node CREATED="1595835435786" FOLDED="true" ID="ID_1381917212" MODIFIED="1598777701475" TEXT="5">
 <node CREATED="1595835451128" ID="ID_251420346" MODIFIED="1595835457089" TEXT="catalytic"/>
 </node>
 </node>
