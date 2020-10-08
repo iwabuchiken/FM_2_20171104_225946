bc325cd63f85f74b2744bf3fd415b3ad0116eed9
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-08 18:24:53 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/37_miscs/W920mathematics/W920_mathematics.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1602137249919" ID="ID_756532476" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1602137283898" TEXT="W920:mathematics">
+<node CREATED="1602137296650" ID="ID_202782793" LINK="https://ja.wikipedia.org/wiki/&#x7279;&#x6b8a;&#x7dda;&#x578b;&#x7fa4;" MODIFIED="1602143777952" POSITION="right" TEXT="1. &#x7279;&#x6b8a;&#x7dda;&#x578b;&#x7fa4;">
+<node CREATED="1602137329753" ID="ID_1492881769" LINK="https://ja.wikipedia.org/wiki/&#x7fa4;_(&#x6570;&#x5b66;)" MODIFIED="1602143803992" TEXT="2. &#x7fa4;_(&#x6570;&#x5b66;)">
+<node CREATED="1602137360794" ID="ID_1381099069" LINK="https://ja.wikipedia.org/wiki/&#x5358;&#x4f4d;&#x5143;" MODIFIED="1602143807223" TEXT="3. &#x5358;&#x4f4d;&#x5143;">
+<node CREATED="1602138552451" ID="ID_111497917" LINK="https://ja.wikipedia.org/wiki/&#x30de;&#x30b0;&#x30de;_(&#x6570;&#x5b66;)" MODIFIED="1602143795624" TEXT="4. &#x30de;&#x30b0;&#x30de;_(&#x6570;&#x5b66;)"/>
+</node>
+<node CREATED="1602140566746" ID="ID_664178936" LINK="https://ja.wikipedia.org/wiki/&#x30e2;&#x30ce;&#x30a4;&#x30c9;" MODIFIED="1602143801539" TEXT="5. &#x30e2;&#x30ce;&#x30a4;&#x30c9;"/>
+</node>
+</node>
+</node>
+</map>
