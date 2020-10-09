b1812a6fad7d026dbc91bfb989029fc542d9c073
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-09 17:19:07 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/37_miscs/W920mathematics/W920_mathematics.mm
+++ Projects/JVEMV6/37_miscs/W920mathematics/W920_mathematics.mm
@@ -7,6 +7,30 @@
 <node CREATED="1602138552451" ID="ID_111497917" LINK="https://ja.wikipedia.org/wiki/&#x30de;&#x30b0;&#x30de;_(&#x6570;&#x5b66;)" MODIFIED="1602143795624" TEXT="4. &#x30de;&#x30b0;&#x30de;_(&#x6570;&#x5b66;)"/>
 </node>
 <node CREATED="1602140566746" ID="ID_664178936" LINK="https://ja.wikipedia.org/wiki/&#x30e2;&#x30ce;&#x30a4;&#x30c9;" MODIFIED="1602143801539" TEXT="5. &#x30e2;&#x30ce;&#x30a4;&#x30c9;"/>
+<node CREATED="1602227026579" ID="ID_466981427" LINK="https://ja.wikipedia.org/wiki/&#x6b63;&#x5247;&#x884c;&#x5217;" MODIFIED="1602227035689" TEXT="6. &#x6b63;&#x5247;&#x884c;&#x5217;">
+<node CREATED="1602229286257" ID="ID_480111165" LINK="https://ja.wikipedia.org/wiki/&#x884c;&#x5217;&#x5f0f;" MODIFIED="1602229314767" TEXT="7. &#x884c;&#x5217;&#x5f0f;"/>
+</node>
+<node CREATED="1602230922290" ID="ID_214987108" LINK="https://ja.wikipedia.org/wiki/&#x4e00;&#x822c;&#x7dda;&#x578b;&#x7fa4;" MODIFIED="1602230938516" TEXT="8. &#x4e00;&#x822c;&#x7dda;&#x578b;&#x7fa4;"/>
+</node>
+</node>
+<node CREATED="1602228452043" ID="ID_1319859973" MODIFIED="1602228456006" POSITION="left" TEXT="scilab">
+<node CREATED="1602228457246" ID="ID_1483754357" MODIFIED="1602228462335" TEXT="tutorial">
+<node CREATED="1602228462746" ID="ID_1984345391" MODIFIED="1602228464183" TEXT="general">
+<node CREATED="1602228465047" ID="ID_173706245" LINK="https://help.scilab.org/docs/6.1.0/en_US/matrix.html" MODIFIED="1602228468297" TEXT="1"/>
+<node CREATED="1602228465695" ID="ID_1418074925" LINK="https://x-engineer.org/graduate-engineering/programming-languages/scilab/extracting-data-from-a-matrix-in-scilab/" MODIFIED="1602228621592" TEXT="2"/>
+<node CREATED="1602228466299" ID="ID_1887691052" MODIFIED="1602228466792" TEXT="3"/>
+</node>
+<node CREATED="1602228769410" ID="ID_1545958232" MODIFIED="1602228773526" TEXT="apis">
+<node CREATED="1602228774794" ID="ID_1095031328" MODIFIED="1602228776919" TEXT="transpose">
+<node CREATED="1602228777849" ID="ID_1572141988" LINK="https://stackoverflow.com/questions/13604961/scilab-syntax-how-to-transpose-and-reshape-without-the-use-of-an-intermediate-v" MODIFIED="1602228781811" TEXT="1"/>
+<node CREATED="1602228778497" ID="ID_1770644774" MODIFIED="1602228778879" TEXT="2"/>
+<node CREATED="1602228779506" ID="ID_674252472" MODIFIED="1602228779759" TEXT="3"/>
+</node>
+<node CREATED="1602229343745" ID="ID_1974057560" MODIFIED="1602229350782" TEXT="determinant"/>
+</node>
+</node>
+<node CREATED="1602229096777" ID="ID_1391831897" MODIFIED="1602229098414" TEXT="manual">
+<node CREATED="1602229099250" ID="ID_327111855" LINK="https://help.scilab.org/docs/5.3.3/en_US/string.html" MODIFIED="1602229113359" TEXT="1. help.scilab.org/docs"/>
 </node>
 </node>
 </node>
