670640f6b7726668b8e0c6fcc968b2dfbdd32e11
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-21 17:08:18 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/37_miscs/21_math-physics/7_fractal.mm
@@ -0,0 +1,36 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1603128106572" ID="ID_1288595720" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1603262308246" TEXT="7_fractal.mm">
+<node CREATED="1603262315772" ID="ID_1574688227" LINK="https://ja.wikipedia.org/wiki/&#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;" MODIFIED="1603262330668" POSITION="right" TEXT="1. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;">
+<node CREATED="1603262529782" ID="ID_1207975362" MODIFIED="1603262554941" TEXT="2. &#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6b21;&#x5143;">
+<node CREATED="1603264090441" ID="ID_1363411492" LINK="https://en.wikipedia.org/wiki/Hausdorff_measure" MODIFIED="1603267010856" TEXT="6. &#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6e2c;&#x5ea6; #">
+<node CREATED="1603264239462" ID="ID_1501500845" LINK="https://en.wikipedia.org/wiki/Outer_measure" MODIFIED="1603264769392" TEXT="6-1. outer measure"/>
+<node CREATED="1603264423858" ID="ID_88940399" MODIFIED="1603264774242" TEXT="6-2. geometric measure theory"/>
+<node CREATED="1603264599765" ID="ID_1322096825" LINK="https://en.wikipedia.org/wiki/Measure_(mathematics)" MODIFIED="1603264777556" TEXT="6-3. Measure theory">
+<node CREATED="1603264698283" ID="ID_208914709" LINK="https://en.wikipedia.org/wiki/Sigma_additivity" MODIFIED="1603264783439" TEXT="7.  countably additive">
+<node CREATED="1603264747609" ID="ID_1898901373" MODIFIED="1603264787594" TEXT="8. additivity"/>
+<node CREATED="1603265854657" ID="ID_1363384903" LINK="https://en.wikipedia.org/wiki/Extended_real_number_line" MODIFIED="1603265869164" TEXT="9. extended real number line"/>
+<node CREATED="1603266052573" ID="ID_232257533" LINK="https://ja.wikipedia.org/wiki/&#x5b8c;&#x5168;&#x52a0;&#x6cd5;&#x7684;&#x96c6;&#x5408;&#x95a2;&#x6570;" MODIFIED="1603266061797" TEXT="7-1. &#x5b8c;&#x5168;&#x52a0;&#x6cd5;&#x7684;&#x96c6;&#x5408;&#x95a2;&#x6570;"/>
+<node CREATED="1603266184166" ID="ID_1335469968" LINK="https://en.wikipedia.org/wiki/Disjoint_sets" MODIFIED="1603266194404" TEXT="10. Disjoint sets"/>
+<node CREATED="1603266445475" ID="ID_1726547065" LINK="https://en.wikipedia.org/wiki/Field_of_sets" MODIFIED="1603266595093" TEXT="11. algebra of sets "/>
+<node CREATED="1603266564638" ID="ID_225176992" LINK="https://en.wikipedia.org/wiki/&#x3a3;-algebra" MODIFIED="1603266599229" TEXT="12. &#x3c3;-algebra"/>
+</node>
+</node>
+</node>
+<node CREATED="1603266929813" ID="ID_659384791" LINK="https://ja.wikipedia.org/wiki/&#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;" MODIFIED="1603267034829" TEXT="13. &#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;"/>
+<node CREATED="1603266857054" ID="ID_1486959122" MODIFIED="1603266858635" TEXT="texts">
+<node CREATED="1603266860004" ID="ID_943194604" MODIFIED="1603266871032" TEXT="#1 &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x5e7e;&#x4f55;&#x5b66;&#x306b;&#x304a;&#x3051;&#x308b;"/>
+<node CREATED="1603266861166" ID="ID_1728400149" MODIFIED="1603266885228" TEXT="#2 &#x521d;&#x7b49;&#x5e7e;&#x4f55;&#x5b66;&#x3067;&#x7528;&#x3044;&#x3089;&#x308c;&#x308b;&#x901a;&#x5e38;&#x306e;"/>
+</node>
+</node>
+<node CREATED="1603262539998" ID="ID_1749328146" MODIFIED="1603262558160" TEXT="3. &#x4f4d;&#x76f8;&#x6b21;&#x5143;"/>
+<node CREATED="1603262545831" ID="ID_389137728" MODIFIED="1603262561975" TEXT="4. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x6b21;&#x5143;"/>
+<node CREATED="1603263540132" ID="ID_1163858969" MODIFIED="1603263546309" TEXT="explain">
+<node CREATED="1603263547320" ID="ID_579358838" LINK="https://hr-inoue.net/zscience/topics/fractal/fractal.html" MODIFIED="1603264116794" TEXT="e-1. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x306e;&#x8a71;"/>
+<node CREATED="1603263548071" ID="ID_1214463540" LINK="https://gakuen.gifu-net.ed.jp/~contents/museum/frac/page22_0.html" MODIFIED="1603264118737" TEXT="e-2. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x56f3;&#x5f62;&#x306b;&#x3061;&#x306a;&#x3093;&#x3060;&#x8a71;&#x984c;&#x3092;"/>
+<node CREATED="1603263548969" ID="ID_1317668502" MODIFIED="1603264121737" TEXT="e-3"/>
+</node>
+<node CREATED="1603263573804" ID="ID_1945546229" LINK="https://ja.wikipedia.org/wiki/&#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x5e7e;&#x4f55;" MODIFIED="1603263582272" TEXT="5. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x5e7e;&#x4f55;"/>
+</node>
+</node>
+</map>
