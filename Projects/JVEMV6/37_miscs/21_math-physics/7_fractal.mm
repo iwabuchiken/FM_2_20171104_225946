4ae7a80754c7d64952279985d4b97055d93fe559
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-22 15:45:39 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/37_miscs/21_math-physics/7_fractal.mm
+++ Projects/JVEMV6/37_miscs/21_math-physics/7_fractal.mm
@@ -2,12 +2,21 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1603128106572" ID="ID_1288595720" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1603262308246" TEXT="7_fractal.mm">
 <node CREATED="1603262315772" ID="ID_1574688227" LINK="https://ja.wikipedia.org/wiki/&#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;" MODIFIED="1603262330668" POSITION="right" TEXT="1. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;">
-<node CREATED="1603262529782" ID="ID_1207975362" LINK="https://ja.wikipedia.org/wiki/&#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6b21;&#x5143;" MODIFIED="1603288026680" TEXT="2. &#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6b21;&#x5143;">
-<node CREATED="1603264090441" ID="ID_1363411492" LINK="https://en.wikipedia.org/wiki/Hausdorff_measure" MODIFIED="1603267010856" TEXT="6. &#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6e2c;&#x5ea6; #">
+<node CREATED="1603263540132" ID="ID_1163858969" MODIFIED="1603348911157" TEXT="explain">
+<node CREATED="1603263547320" ID="ID_579358838" LINK="https://hr-inoue.net/zscience/topics/fractal/fractal.html" MODIFIED="1603264116794" TEXT="e-1. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x306e;&#x8a71;"/>
+<node CREATED="1603263548071" ID="ID_1214463540" LINK="https://gakuen.gifu-net.ed.jp/~contents/museum/frac/page22_0.html" MODIFIED="1603264118737" TEXT="e-2. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x56f3;&#x5f62;&#x306b;&#x3061;&#x306a;&#x3093;&#x3060;&#x8a71;&#x984c;&#x3092;"/>
+<node CREATED="1603263548969" ID="ID_1317668502" MODIFIED="1603264121737" TEXT="e-3"/>
+</node>
+<node CREATED="1603348919167" ID="ID_835977892" MODIFIED="1603348921056" TEXT="texts">
+<node CREATED="1603348939299" ID="ID_909079481" MODIFIED="1603348941687" TEXT="#1&#x300c;&#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6b21;&#x5143;&#x304c;&#x4f4d;&#x76f8;&#x6b21;&#x5143;&#x3092;&#x53b3;&#x5bc6;&#x306b;"/>
+<node CREATED="1603348956256" ID="ID_1555445291" MODIFIED="1603348958567" TEXT="#2 &#x5b8c;&#x5168;&#x306b;&#x81ea;&#x5df1;&#x76f8;&#x4f3c;&#x306a;"/>
+</node>
+<node CREATED="1603262529782" FOLDED="true" ID="ID_1207975362" LINK="https://ja.wikipedia.org/wiki/&#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6b21;&#x5143;" MODIFIED="1603347050123" TEXT="2. &#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6b21;&#x5143;">
+<node CREATED="1603264090441" FOLDED="true" ID="ID_1363411492" LINK="https://en.wikipedia.org/wiki/Hausdorff_measure" MODIFIED="1603347050092" TEXT="6. &#x30cf;&#x30a6;&#x30b9;&#x30c9;&#x30eb;&#x30d5;&#x6e2c;&#x5ea6; #">
 <node CREATED="1603264239462" ID="ID_1501500845" LINK="https://en.wikipedia.org/wiki/Outer_measure" MODIFIED="1603264769392" TEXT="6-1. outer measure"/>
 <node CREATED="1603264423858" ID="ID_88940399" MODIFIED="1603264774242" TEXT="6-2. geometric measure theory"/>
-<node CREATED="1603264599765" ID="ID_1322096825" LINK="https://en.wikipedia.org/wiki/Measure_(mathematics)" MODIFIED="1603264777556" TEXT="6-3. Measure theory">
-<node CREATED="1603264698283" ID="ID_208914709" LINK="https://en.wikipedia.org/wiki/Sigma_additivity" MODIFIED="1603264783439" TEXT="7.  countably additive">
+<node CREATED="1603264599765" FOLDED="true" ID="ID_1322096825" LINK="https://en.wikipedia.org/wiki/Measure_(mathematics)" MODIFIED="1603347050092" TEXT="6-3. Measure theory">
+<node CREATED="1603264698283" FOLDED="true" ID="ID_208914709" LINK="https://en.wikipedia.org/wiki/Sigma_additivity" MODIFIED="1603347050092" TEXT="7.  countably additive">
 <node CREATED="1603264747609" ID="ID_1898901373" MODIFIED="1603264787594" TEXT="8. additivity"/>
 <node CREATED="1603265854657" ID="ID_1363384903" LINK="https://en.wikipedia.org/wiki/Extended_real_number_line" MODIFIED="1603265869164" TEXT="9. extended real number line"/>
 <node CREATED="1603266052573" ID="ID_232257533" LINK="https://ja.wikipedia.org/wiki/&#x5b8c;&#x5168;&#x52a0;&#x6cd5;&#x7684;&#x96c6;&#x5408;&#x95a2;&#x6570;" MODIFIED="1603266061797" TEXT="7-1. &#x5b8c;&#x5168;&#x52a0;&#x6cd5;&#x7684;&#x96c6;&#x5408;&#x95a2;&#x6570;"/>
@@ -17,29 +26,39 @@
 </node>
 </node>
 </node>
-<node CREATED="1603266929813" ID="ID_659384791" LINK="https://ja.wikipedia.org/wiki/&#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;" MODIFIED="1603267034829" TEXT="13. &#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;">
+<node CREATED="1603266929813" FOLDED="true" ID="ID_659384791" LINK="https://ja.wikipedia.org/wiki/&#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;" MODIFIED="1603347050092" TEXT="13. &#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;">
 <node CREATED="1603287936196" ID="ID_280076828" MODIFIED="1603287937318" TEXT="#1"/>
 <node CREATED="1603287937560" ID="ID_1819262622" LINK="https://ja.wikipedia.org/wiki/&#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;#&#x3088;&#x308a;&#x8907;&#x96d1;&#x306a;&#x56f3;&#x5f62;&#x3078;&#x306e;&#x62e1;&#x5f35;" MODIFIED="1603287946103" TEXT="#2 &#x30b8;&#x30e7;&#x30eb;&#x30c0;&#x30f3;&#x6e2c;&#x5ea6;#&#x3088;&#x308a;&#x8907;&#x96d1;&#x306a;&#x56f3;&#x5f62;&#x3078;&#x306e;&#x62e1;&#x5f35;"/>
 </node>
-<node CREATED="1603266857054" ID="ID_1486959122" MODIFIED="1603266858635" TEXT="texts">
+<node CREATED="1603266857054" FOLDED="true" ID="ID_1486959122" MODIFIED="1603347050092" TEXT="texts">
 <node CREATED="1603266860004" ID="ID_943194604" MODIFIED="1603266871032" TEXT="#1 &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x5e7e;&#x4f55;&#x5b66;&#x306b;&#x304a;&#x3051;&#x308b;"/>
 <node CREATED="1603266861166" ID="ID_1728400149" MODIFIED="1603266885228" TEXT="#2 &#x521d;&#x7b49;&#x5e7e;&#x4f55;&#x5b66;&#x3067;&#x7528;&#x3044;&#x3089;&#x308c;&#x308b;&#x901a;&#x5e38;&#x306e;"/>
 </node>
-<node CREATED="1603289089105" ID="ID_66349880" LINK="https://en.wikipedia.org/wiki/Hausdorff_dimension" MODIFIED="1603289102015" TEXT="2-1. Hausdorff_dimension">
-<node CREATED="1603289182936" ID="ID_1485434532" MODIFIED="1603289184234" TEXT="texts">
+<node COLOR="#338800" CREATED="1603289089105" FOLDED="true" ID="ID_66349880" LINK="https://en.wikipedia.org/wiki/Hausdorff_dimension" MODIFIED="1603347050108" TEXT="2-1. Hausdorff_dimension">
+<node CREATED="1603289182936" FOLDED="true" ID="ID_1485434532" MODIFIED="1603347050108" TEXT="texts">
 <node CREATED="1603289179672" ID="ID_1128222581" MODIFIED="1603289181894" TEXT="#1 In mathematics, Hausdorff dimension is"/>
 <node CREATED="1603289196149" ID="ID_307754375" MODIFIED="1603289198417" TEXT="#2 However, formulas have also been"/>
 <node CREATED="1603289459918" ID="ID_390236922" MODIFIED="1603289462532" TEXT="#3 In mathematical terms, the Hausdorff dimension generalizes "/>
+<node CREATED="1603344520065" ID="ID_990411652" MODIFIED="1603344523310" TEXT="#4 For instance, the Koch snowflake "/>
+</node>
+<node CREATED="1603344125731" ID="ID_1623220250" LINK="https://en.wikipedia.org/wiki/Vector_space" MODIFIED="1603344286567" TEXT="14. Vector_space"/>
+<node CREATED="1603344287386" ID="ID_1503680874" LINK="https://en.wikipedia.org/wiki/Inner_product_space" MODIFIED="1603344319635" TEXT="15. Inner_product_space"/>
+<node CREATED="1603344733986" ID="ID_660850202" LINK="https://en.wikipedia.org/wiki/Axiom_of_regularity" MODIFIED="1603344749478" TEXT="16. irregular sets (Axiom_of_regularity)"/>
+<node CREATED="1603346824211" ID="ID_125618870" MODIFIED="1603346851442" TEXT="17. Minkowski&#x2013;Bouligand dimension"/>
 </node>
 </node>
+<node CREATED="1603262539998" ID="ID_1749328146" LINK="https://ja.wikipedia.org/wiki/&#x30eb;&#x30d9;&#x30fc;&#x30b0;&#x88ab;&#x8986;&#x6b21;&#x5143;" MODIFIED="1603347175658" TEXT="3. &#x4f4d;&#x76f8;&#x6b21;&#x5143;">
+<node CREATED="1603347385566" ID="ID_824718821" LINK="https://ja.wikipedia.org/wiki/&#x4f4d;&#x76f8;&#x7a7a;&#x9593;" MODIFIED="1603347435649" TEXT="3-1. &#x4f4d;&#x76f8;&#x7a7a;&#x9593;">
+<node CREATED="1603347818644" ID="ID_91078830" MODIFIED="1603347820921" TEXT="texts">
+<node CREATED="1603347821708" ID="ID_233718886" MODIFIED="1603347824393" TEXT="#1 &#x4f4d;&#x76f8;&#x7a7a;&#x9593;&#x306e;&#x53b3;&#x5bc6;&#x306a;&#x5b9a;&#x7fa9;&#x306f;"/>
 </node>
-<node CREATED="1603262539998" ID="ID_1749328146" MODIFIED="1603262558160" TEXT="3. &#x4f4d;&#x76f8;&#x6b21;&#x5143;"/>
+<node CREATED="1603347936056" ID="ID_1894986577" LINK="https://ja.wikipedia.org/wiki/&#x3079;&#x304d;&#x96c6;&#x5408;" MODIFIED="1603347963101" TEXT="18. &#x3079;&#x304d;&#x96c6;&#x5408;"/>
+<node CREATED="1603348633873" ID="ID_1724247044" MODIFIED="1603348654138" TEXT="18-1. &#x958b;&#x96c6;&#x5408;"/>
+<node CREATED="1603348658169" ID="ID_831590702" MODIFIED="1603348665050" TEXT="18-2. &#x53f0;&#x96c6;&#x5408;"/>
+</node>
+<node CREATED="1603347409199" ID="ID_1965320547" LINK="https://ja.wikipedia.org/wiki/&#x4f4d;&#x76f8;&#x7684;&#x6027;&#x8cea;" MODIFIED="1603347465394" TEXT="3-2. &#x4f4d;&#x76f8;&#x4e0d;&#x5909;&#x91cf;"/>
+</node>
 <node CREATED="1603262545831" ID="ID_389137728" MODIFIED="1603262561975" TEXT="4. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x6b21;&#x5143;"/>
-<node CREATED="1603263540132" ID="ID_1163858969" MODIFIED="1603263546309" TEXT="explain">
-<node CREATED="1603263547320" ID="ID_579358838" LINK="https://hr-inoue.net/zscience/topics/fractal/fractal.html" MODIFIED="1603264116794" TEXT="e-1. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x306e;&#x8a71;"/>
-<node CREATED="1603263548071" ID="ID_1214463540" LINK="https://gakuen.gifu-net.ed.jp/~contents/museum/frac/page22_0.html" MODIFIED="1603264118737" TEXT="e-2. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x56f3;&#x5f62;&#x306b;&#x3061;&#x306a;&#x3093;&#x3060;&#x8a71;&#x984c;&#x3092;"/>
-<node CREATED="1603263548969" ID="ID_1317668502" MODIFIED="1603264121737" TEXT="e-3"/>
-</node>
 <node CREATED="1603263573804" ID="ID_1945546229" LINK="https://ja.wikipedia.org/wiki/&#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x5e7e;&#x4f55;" MODIFIED="1603263582272" TEXT="5. &#x30d5;&#x30e9;&#x30af;&#x30bf;&#x30eb;&#x5e7e;&#x4f55;"/>
 </node>
 </node>
