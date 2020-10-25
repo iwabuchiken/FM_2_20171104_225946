bcc27c27a292fdf3c3430a00076b8f50748fed25
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-25 22:55:31 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/37_miscs/21_math-physics/8_gauge-theory.mm
@@ -0,0 +1,27 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1603632218717" ID="ID_421414033" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1603632281232" TEXT="8. gauge theory">
+<node CREATED="1603632317663" ID="ID_242266010" LINK="https://ja.wikipedia.org/wiki/&#x30b2;&#x30fc;&#x30b8;&#x7406;&#x8ad6;" MODIFIED="1603632606738" POSITION="right" TEXT="1. &#x30b2;&#x30fc;&#x30b8;&#x7406;&#x8ad6;">
+<node CREATED="1603632610683" ID="ID_1454408122" MODIFIED="1603632612529" TEXT="fur">
+<node CREATED="1603633159977" ID="ID_258774376" LINK="https://ja.wikipedia.org/wiki/&#x30e9;&#x30b0;&#x30e9;&#x30f3;&#x30b8;&#x30a2;&#x30f3;_(&#x5834;&#x306e;&#x7406;&#x8ad6;)" MODIFIED="1603633171377" TEXT="2. &#x30e9;&#x30b0;&#x30e9;&#x30f3;&#x30b8;&#x30a2;&#x30f3;_(&#x5834;&#x306e;&#x7406;&#x8ad6;)">
+<node CREATED="1603356800076" ID="ID_431729473" MODIFIED="1603608971013" TEXT="@@@">
+<cloud/>
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+<node CREATED="1603633337917" ID="ID_221017844" MODIFIED="1603633339041" TEXT="fur">
+<node CREATED="1603633339829" ID="ID_1756042240" MODIFIED="1603633349802" TEXT="3. &#x53e4;&#x5178;&#x5834;&#x7406;&#x8ad6;"/>
+<node CREATED="1603633356961" ID="ID_268862416" MODIFIED="1603633363293" TEXT="3-1. &#x30e9;&#x30b0;&#x30e9;&#x30f3;&#x30b8;&#x30e5;&#x529b;&#x5b66;"/>
+<node CREATED="1603633379113" ID="ID_1866640443" MODIFIED="1603633382794" TEXT="3-2. &#x81ea;&#x7531;&#x5ea6;"/>
+<node CREATED="1603633389781" ID="ID_483662654" MODIFIED="1603633393588" TEXT="3-3. &#x9023;&#x7d9a;&#x4f53;"/>
+</node>
+</node>
+<node CREATED="1603633239103" ID="ID_897754000" MODIFIED="1603633246193" TEXT="2-1. &#x5c40;&#x6240;&#x5909;&#x63db;">
+<node CREATED="1603633295428" ID="ID_1061688142" MODIFIED="1603633297244" TEXT="search">
+<node CREATED="1603633298126" ID="ID_865262848" MODIFIED="1603633307048" TEXT="&#x5c40;&#x6240;&#x5909;&#x63db; &#x7269;&#x7406;&#x5b66;"/>
+</node>
+</node>
+<node CREATED="1603633419321" ID="ID_1285781428" LINK="https://ja.wikipedia.org/wiki/&#x5834;&#x306e;&#x91cf;&#x5b50;&#x8ad6;" MODIFIED="1603633439757" TEXT="2-2. &#x5834;&#x306e;&#x7406;&#x8ad6;"/>
+</node>
+</node>
+</node>
+</map>
