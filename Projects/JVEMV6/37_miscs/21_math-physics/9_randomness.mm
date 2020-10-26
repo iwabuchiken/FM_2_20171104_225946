51c10e630ccabfa840583bd60a417a48e9c4b6a1
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-26 15:40:11 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/37_miscs/21_math-physics/9_randomness.mm
@@ -0,0 +1,24 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1603691767932" ID="ID_918114952" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1603691830372" TEXT="9. randomness">
+<node CREATED="1603691850941" ID="ID_1683820467" LINK="https://ja.wikipedia.org/wiki/&#x30e9;&#x30f3;&#x30c0;&#x30e0;" MODIFIED="1603691857198" POSITION="right" TEXT="1. &#x30e9;&#x30f3;&#x30c0;&#x30e0;">
+<node CREATED="1603691872752" ID="ID_727501092" MODIFIED="1603691875482" TEXT="search">
+<node CREATED="1603691876098" ID="ID_94920643" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#x60c5;&#x5831;&#x7406;&#x8ad6;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&amp;ia=web" MODIFIED="1603691899982" TEXT="1. q=&#x60c5;&#x5831;&#x7406;&#x8ad6;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;"/>
+</node>
+<node CREATED="1603691941960" ID="ID_1388761674" MODIFIED="1603691944059" TEXT="texts">
+<node COLOR="#338800" CREATED="1603691944060" ID="ID_689820969" MODIFIED="1603693303755" TEXT="#1"/>
+<node COLOR="#338800" CREATED="1603691945314" ID="ID_848523978" MODIFIED="1603693459621" TEXT="#2 &#x4e8b;&#x8c61;&#x30fb;&#x8a18;&#x53f7;&#x306a;&#x3069;&#x306e;&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x306a;&#x5217;&#x306b;&#x306f;"/>
+<node COLOR="#338800" CREATED="1603693445090" ID="ID_154825962" MODIFIED="1603693489620" TEXT="#3 &#x6570;&#x5b66;&#x3001;&#x78ba;&#x7387;&#x3001;&#x7d71;&#x8a08;&#x306e;&#x5206;&#x91ce;&#x3067;&#x306f;"/>
+<node CREATED="1603693486086" ID="ID_1649315391" MODIFIED="1603693488085" TEXT="#4 &#x78ba;&#x7387;&#x5909;&#x6570;&#x306e;&#x5217;&#x3092;">
+<node CREATED="1603356800076" ID="ID_431729473" MODIFIED="1603693799247" TEXT="@@@">
+<cloud/>
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1603692636563" ID="ID_557702357" MODIFIED="1603692642625" POSITION="left" TEXT="spreadsheets">
+<node CREATED="1603692649230" ID="ID_1169494083" LINK="C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\21_math&amp;physics\9_randomness\res_[37#21.9_math].[theme=random].[session-1~].ods" MODIFIED="1603693226748" TEXT="1"/>
+</node>
+</node>
+</map>
