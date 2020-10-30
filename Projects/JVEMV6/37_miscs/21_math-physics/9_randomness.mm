7da8df8da110dea9de7bdd54280be5b2feb9d179
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-10-30 16:41:30 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/37_miscs/21_math-physics/9_randomness.mm
+++ Projects/JVEMV6/37_miscs/21_math-physics/9_randomness.mm
@@ -3,19 +3,55 @@
 <node CREATED="1603691767932" ID="ID_918114952" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1603691830372" TEXT="9. randomness">
 <node CREATED="1603691850941" ID="ID_1683820467" LINK="https://ja.wikipedia.org/wiki/&#x30e9;&#x30f3;&#x30c0;&#x30e0;" MODIFIED="1603691857198" POSITION="right" TEXT="1. &#x30e9;&#x30f3;&#x30c0;&#x30e0;">
 <node CREATED="1603691872752" ID="ID_727501092" MODIFIED="1603691875482" TEXT="search">
-<node CREATED="1603691876098" ID="ID_94920643" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#x60c5;&#x5831;&#x7406;&#x8ad6;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&amp;ia=web" MODIFIED="1603691899982" TEXT="1. q=&#x60c5;&#x5831;&#x7406;&#x8ad6;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;"/>
+<node CREATED="1603691876098" ID="ID_94920643" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#x60c5;&#x5831;&#x7406;&#x8ad6;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&amp;ia=web" MODIFIED="1604038695373" TEXT="s-1.1 q=&#x60c5;&#x5831;&#x7406;&#x8ad6;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;"/>
+<node CREATED="1604038329643" ID="ID_1535185961" LINK="https://3g2upl4pq6kufc4m.onion/?q=binary+sequence+randomness&amp;ia=web" MODIFIED="1604038712743" TEXT="s-1.2 binary sequence randomness"/>
+<node CREATED="1604038484578" ID="ID_385572853" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#x4e8c;&#x5024;&#x6570;&#x5217;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&#x5224;&#x5b9a;&amp;ia=web" MODIFIED="1604038723384" TEXT="s-1.3 &#x4e8c;&#x5024;&#x6570;&#x5217;+&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&#x5224;&#x5b9a;"/>
+<node CREATED="1604038835778" ID="ID_354817421" LINK="https://3g2upl4pq6kufc4m.onion/?q=randomness+test+binary&amp;ia=web" MODIFIED="1604038844735" TEXT="s-1.4 randomness+test+binary"/>
+<node CREATED="1604038900145" ID="ID_1986626013" LINK="https://3g2upl4pq6kufc4m.onion/?q=randomness+test&amp;ia=web" MODIFIED="1604038907727" TEXT="s-1.5 randomness+test"/>
+<node CREATED="1604039390601" ID="ID_1977017887" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&#x5224;&#x5b9a;&amp;ia=web" MODIFIED="1604039406847" TEXT="s-1.6. &#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&#x5224;&#x5b9a;"/>
 </node>
 <node CREATED="1603691941960" ID="ID_1388761674" MODIFIED="1603691944059" TEXT="texts">
 <node COLOR="#338800" CREATED="1603691944060" ID="ID_689820969" MODIFIED="1603693303755" TEXT="#1"/>
 <node COLOR="#338800" CREATED="1603691945314" ID="ID_848523978" MODIFIED="1603693459621" TEXT="#2 &#x4e8b;&#x8c61;&#x30fb;&#x8a18;&#x53f7;&#x306a;&#x3069;&#x306e;&#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x306a;&#x5217;&#x306b;&#x306f;"/>
 <node COLOR="#338800" CREATED="1603693445090" ID="ID_154825962" MODIFIED="1603693489620" TEXT="#3 &#x6570;&#x5b66;&#x3001;&#x78ba;&#x7387;&#x3001;&#x7d71;&#x8a08;&#x306e;&#x5206;&#x91ce;&#x3067;&#x306f;"/>
-<node CREATED="1603693486086" ID="ID_1649315391" MODIFIED="1603693488085" TEXT="#4 &#x78ba;&#x7387;&#x5909;&#x6570;&#x306e;&#x5217;&#x3092;">
+<node COLOR="#338800" CREATED="1603693486086" ID="ID_1649315391" MODIFIED="1604036882690" TEXT="#4 &#x78ba;&#x7387;&#x5909;&#x6570;&#x306e;&#x5217;&#x3092;">
 <node CREATED="1603356800076" ID="ID_431729473" MODIFIED="1603693799247" TEXT="@@@">
 <cloud/>
 <font NAME="SansSerif" SIZE="20"/>
 </node>
 </node>
+<node COLOR="#338800" CREATED="1604036877597" ID="ID_1655136641" MODIFIED="1604036965244" TEXT="#5 &#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x6027;&#x306f;&#x3001;&#x3088;&#x304f;&#x5b9a;&#x7fa9;&#x3055;&#x308c;&#x305f;"/>
+<node CREATED="1604036962163" ID="ID_793750636" MODIFIED="1604036963974" TEXT="#6 &#x7121;&#x4f5c;&#x70ba;&#x62bd;&#x51fa;(random selection)&#x306f;&#x3001;"/>
 </node>
+<node CREATED="1604038646611" ID="ID_1121034358" MODIFIED="1604038652952" TEXT="fur">
+<node CREATED="1604038653994" ID="ID_719896597" MODIFIED="1604038737095" TEXT="2. randomness tests">
+<node CREATED="1604038743882" ID="ID_1399337751" LINK="https://en.wikipedia.org/wiki/CUSUM" MODIFIED="1604039058958" TEXT="2-1. CUSUM">
+<node CREATED="1604038952393" ID="ID_820377128" MODIFIED="1604038953774" TEXT="fur">
+<node CREATED="1604038954561" ID="ID_966862065" LINK="https://blogs.sas.com/content/iml/2019/04/22/cusum-test-randomness-binary-sequence.html" MODIFIED="1604039029286" TEXT="2-1-1.  The CUSUM test for randomness of a binary sequence "/>
+</node>
+</node>
+<node CREATED="1604039066889" ID="ID_1033011110" LINK="https://en.wikipedia.org/wiki/Randomness_tests" MODIFIED="1604039074782" TEXT="2-2. Randomness_tests"/>
+<node CREATED="1604039278722" ID="ID_958621390" MODIFIED="1604039282043" TEXT="docs">
+<node CREATED="1604039282045" ID="ID_1080056226" LINK="http://daas.la.coocan.jp/GLM/8_nichi_data.htm" MODIFIED="1604039299935" TEXT="doc-2.1. 2&#x5024;&#x30c7;&#x30fc;&#x30bf;&#x306e;&#x89e3;&#x6790;"/>
+<node CREATED="1604039334122" ID="ID_1441017239" LINK="https://ipsj.ixsq.nii.ac.jp/ej/?action=repository_action_common_download&amp;item_id=73958&amp;item_no=1&amp;attribute_id=1&amp;file_no=1" MODIFIED="1604039355914" TEXT="doc-2.2. RMT&#x516c;&#x5f0f;&#x3092;&#x7528;&#x3044;&#x305f;&#x4e71;&#x6570;&#x5ea6;&#x8a55;&#x4fa1;&#x6cd5;&#x306e;&#x63d0;&#x6848;"/>
+<node CREATED="1604039366122" ID="ID_1173891019" LINK="https://www.nli-research.co.jp/report/detail/id=52138?site=nli" MODIFIED="1604040606359" TEXT="doc-2.3. &#x30e9;&#x30f3;&#x30c0;&#x30e0;&#x304b;&#x3069;&#x3046;&#x304b;&#x306e;&#x5224;&#x65ad;">
+<node CREATED="1604040607618" ID="ID_1958113102" MODIFIED="1604040609215" TEXT="texts">
+<node CREATED="1604040620266" ID="ID_351376093" MODIFIED="1604040621135" TEXT="#1"/>
+<node CREATED="1604040621426" ID="ID_1190101498" MODIFIED="1604040623927" TEXT="#2 &#x3053;&#x306e;&#x3088;&#x3046;&#x306b;2&#x7a2e;&#x985e;&#x306e;&#x6587;&#x5b57;&#x304c;"/>
+</node>
+<node CREATED="1604040958635" ID="ID_158241998" MODIFIED="1604040961551" TEXT="search">
+<node CREATED="1604040964771" ID="ID_242829282" MODIFIED="1604040995455" TEXT="s-2. &#x30a6;&#x30a3;&#x30eb;&#x30b3;&#x30af;&#x30bd;&#x30f3;&#x306e;&#x9806;&#x4f4d;&#x548c;&#x691c;&#x5b9a;"/>
+</node>
+</node>
+<node CREATED="1604041180506" ID="ID_799569728" LINK="https://tsapps.nist.gov/publication/get_pdf.cfm?pub_id=151216" MODIFIED="1604041205215" TEXT="doc-2.4. Randomness Testing of the Advanced Encryption Standard Finalist Candidates"/>
+</node>
+<node CREATED="1604040447315" ID="ID_1817854516" LINK="https://stackoverflow.com/questions/3097949/how-can-i-determine-the-statistical-randomness-of-a-binary-string" MODIFIED="1604041151554" TEXT="2-3. How can I determine the statistical randomness of a binary string?"/>
+<node CREATED="1604041297914" ID="ID_550180724" LINK="https://en.wikipedia.org/wiki/Diehard_tests" MODIFIED="1604041305598" TEXT="2-4. Diehard_tests"/>
+</node>
+</node>
+</node>
+<node CREATED="1604040188378" ID="ID_696522737" MODIFIED="1604040191511" POSITION="right" TEXT="interests">
+<node CREATED="1604040197890" ID="ID_1002793361" LINK="https://en.wikipedia.org/wiki/Algorithmic_information_theory" MODIFIED="1604040206255" TEXT="i-1. Algorithmic_information_theory"/>
 </node>
 <node CREATED="1603692636563" ID="ID_557702357" MODIFIED="1603692642625" POSITION="left" TEXT="spreadsheets">
 <node CREATED="1603692649230" ID="ID_1169494083" LINK="C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\37_miscs\21_math&amp;physics\9_randomness\res_[37#21.9_math].[theme=random].[session-1~].ods" MODIFIED="1603693226748" TEXT="1"/>
