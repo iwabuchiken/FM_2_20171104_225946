c71d3e67f03681132ab6c6ef9d95a98bda681b95
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-05 11:40:08 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/37_miscs/20_philosophy/4_japanese-history/4_japanese-history.mm
+++ Projects/JVEMV6/37_miscs/20_philosophy/4_japanese-history/4_japanese-history.mm
@@ -1,16 +1,16 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1604219829243" ID="ID_611530253" LINK="../20_philosophy.mm" MODIFIED="1604390901240" TEXT="4. japanese history">
-<node CREATED="1604219840218" MODIFIED="1604219865911" POSITION="right" TEXT="1. 2nd-world-war">
-<node CREATED="1604219867104" MODIFIED="1604219884870" TEXT="1. &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;">
-<node CREATED="1604219887107" MODIFIED="1604219894097" TEXT="docs">
-<node CREATED="1604220053755" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;" MODIFIED="1604220097321" TEXT="1. &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044; (wi)">
+<node CREATED="1604219840218" ID="ID_1087520442" MODIFIED="1604219865911" POSITION="right" TEXT="1. 2nd-world-war">
+<node CREATED="1604219867104" ID="ID_324305582" MODIFIED="1604219884870" TEXT="1. &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;">
+<node CREATED="1604219887107" ID="ID_1788330577" MODIFIED="1604219894097" TEXT="docs">
+<node CREATED="1604220053755" ID="ID_965239931" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;" MODIFIED="1604220097321" TEXT="1. &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044; (wi)">
 <node CREATED="1604220101868" ID="ID_1259988080" MODIFIED="1604220104123" TEXT="texts">
 <node COLOR="#338800" CREATED="1604220056096" MODIFIED="1604221541001" TEXT="#1"/>
 <node COLOR="#338800" CREATED="1604220059580" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x80cc;&#x666f;" MODIFIED="1604221541002" TEXT="#2 &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x80cc;&#x666f;"/>
 <node COLOR="#338800" CREATED="1604221311181" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x6226;&#x95d8;&#x306e;&#x7d4c;&#x904e;" MODIFIED="1604221541002" TEXT="#3 &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x6226;&#x95d8;&#x306e;&#x7d4c;&#x904e;"/>
 <node COLOR="#338800" CREATED="1604220713435" MODIFIED="1604389865306" TEXT="#4 &#x4e0a;&#x9678;&#x5f53;&#x521d;&#x3001;&#x6700;&#x3082;&#x6575;&#x306b;"/>
-<node COLOR="#338800" CREATED="1604298705479" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x7b2c;&#x4e00;&#x6b21;&#x30bd;&#x30ed;&#x30e2;&#x30f3;&#x6d77;&#x6226;" MODIFIED="1604390411325" TEXT="#5 &#x7b2c;&#x4e00;&#x6b21;&#x30bd;&#x30ed;&#x30e2;&#x30f3;&#x6d77;&#x6226;"/>
+<node COLOR="#338800" CREATED="1604298705479" ID="ID_693092181" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x7b2c;&#x4e00;&#x6b21;&#x30bd;&#x30ed;&#x30e2;&#x30f3;&#x6d77;&#x6226;" MODIFIED="1604390411325" TEXT="#5 &#x7b2c;&#x4e00;&#x6b21;&#x30bd;&#x30ed;&#x30e2;&#x30f3;&#x6d77;&#x6226;"/>
 <node COLOR="#338800" CREATED="1604390403987" MODIFIED="1604390531844" TEXT="#6 25&#x822a;&#x6226;&#x306f;34&#x6a5f;&#x55aa;&#x5931;&#x306e;"/>
 <node COLOR="#338800" CREATED="1604390527445" ID="ID_1562491111" MODIFIED="1604390718360" TEXT="#7 &#x4e09;&#x5ddd;&#x4e2d;&#x5c06;&#x7387;&#x3044;&#x308b;">
 <node CREATED="1604390796172" ID="ID_1339224540" MODIFIED="1604390798295" TEXT="fur">
@@ -18,7 +18,14 @@
 </node>
 </node>
 <node COLOR="#338800" CREATED="1604390714564" ID="ID_1958394564" MODIFIED="1604391871259" TEXT="#8 &#x4e0a;&#x9678;&#x521d;&#x65e5;&#x304b;&#x3089;&#x306e;"/>
-<node CREATED="1604391866790" ID="ID_679838895" MODIFIED="1604391869615" TEXT="#9 18&#x65e5;&#x3001;&#x540c;&#x5ddd;&#x897f;&#x5cb8;&#x306e;&#x65e5;&#x672c;&#x8ecd;&#x3092;"/>
+<node COLOR="#338800" CREATED="1604391866790" ID="ID_679838895" MODIFIED="1604542057647" TEXT="#9 18&#x65e5;&#x3001;&#x540c;&#x5ddd;&#x897f;&#x5cb8;&#x306e;&#x65e5;&#x672c;&#x8ecd;&#x3092;"/>
+<node COLOR="#338800" CREATED="1604542048731" ID="ID_1366742773" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x30a4;&#x30eb;&#x5ddd;&#x6e21;&#x6cb3;&#x6226;" MODIFIED="1604543244097" TEXT="#10 &#x30a4;&#x30eb;&#x5ddd;&#x6e21;&#x6cb3;&#x6226;"/>
+<node CREATED="1604543239144" ID="ID_1968737936" MODIFIED="1604543242015" TEXT="#11 8&#x6708;18&#x65e5;&#x306b;&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;">
+<node COLOR="#3333ff" CREATED="1603356800076" ID="ID_431729473" MODIFIED="1604543912318" TEXT="@@@">
+<cloud COLOR="#ffff66"/>
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
 </node>
 </node>
 <node CREATED="1604220793028" LINK="https://en.wikipedia.org/wiki/Guadalcanal_campaign#/media/File:Guadalcanal_Aug_7_landings.svg" MODIFIED="1604220812000" TEXT="1.1. Guadalcanal_Aug_7_landings.svg"/>
