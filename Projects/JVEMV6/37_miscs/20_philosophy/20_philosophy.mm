04e37dca081add260206660f2e3fa5392a6d7e7a
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-11 16:49:37 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/37_miscs/20_philosophy/20_philosophy.mm
+++ Projects/JVEMV6/37_miscs/20_philosophy/20_philosophy.mm
@@ -3,7 +3,7 @@
 <node CREATED="1576021347508" ID="ID_1307667782" LINK="../../../JVEMV6.mm" MODIFIED="1585546277178" TEXT="20 philosophy">
 <node CREATED="1577864425389" ID="ID_1718757547" LINK="../../37_A1_&#x4ecf;&#x6559;.mm" MODIFIED="1604565512549" POSITION="right" TEXT="A1 &#x4ecf;&#x6559;"/>
 <node CREATED="1585546284584" ID="ID_25244323" MODIFIED="1585546291412" POSITION="right" TEXT="2 &#x30e1;&#x30bf;&#x502b;&#x7406;&#x5b66;"/>
-<node CREATED="1585547537786" ID="ID_1985840662" MODIFIED="1585547544332" POSITION="right" TEXT="3 afro-american people&apos;s history"/>
+<node CREATED="1605080825316" ID="ID_1555102210" LINK="3_afro-american_peoples-history/3_afro-american_peoples-history.mm" MODIFIED="1605080825347" POSITION="right" TEXT="3 afro-american people&apos;s history"/>
 <node CREATED="1604390901349" ID="ID_488933966" LINK="4_japanese-history/4_japanese-history.mm" MODIFIED="1604390901349" POSITION="right" TEXT="4. japanese history"/>
 <node CREATED="1604565721368" ID="ID_149497451" LINK="5_analytic-philosophy.mm" MODIFIED="1604565721415" POSITION="right" TEXT="5. &#x5206;&#x6790;&#x54f2;&#x5b66;"/>
 <node CREATED="1604813187937" ID="ID_849756492" LINK="6_social-issues/6_social-issues.mm" MODIFIED="1604813187942" POSITION="right" TEXT="6. social issues"/>
---
+++ Projects/JVEMV6/37_miscs/20_philosophy/3_afro-american_peoples-history/3_afro-american_peoples-history.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1585547537786" ID="ID_935957421" LINK="../20_philosophy.mm" MODIFIED="1605080824817" TEXT="3 afro-american people&apos;s history">
+<node CREATED="1605080835900" ID="ID_637540326" MODIFIED="1605080840666" POSITION="right" TEXT="1. people">
+<node CREATED="1605080840667" ID="ID_1274165244" MODIFIED="1605080843678" TEXT="1. Mumia"/>
+</node>
+</node>
+</map>
