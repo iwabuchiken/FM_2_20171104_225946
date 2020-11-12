bb71dd95ad692af19842c44f2e3e7d1269d6b27c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-12 17:21:52 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/60_anthropology-of-religion/60_anthropology-of-religion.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1605151542295" ID="ID_1167411889" LINK="../../JVEMV6.mm" MODIFIED="1605151600117" TEXT="60. anthropology of religion">
+<node CREATED="1605151610643" ID="ID_1933690052" MODIFIED="1605166924920" POSITION="right" TEXT="1. al biruni">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1933690052" ENDARROW="Default" ENDINCLINATION="349;0;" ID="Arrow_ID_938900015" SOURCE="ID_1698654059" STARTARROW="None" STARTINCLINATION="349;0;"/>
+</node>
+<node CREATED="1605166251275" ID="ID_1102695979" MODIFIED="1605166259259" POSITION="right" TEXT="2. start">
+<node CREATED="1605166433309" ID="ID_1396757008" MODIFIED="1605166434585" TEXT="docs">
+<node CREATED="1605166544933" ID="ID_1107876544" LINK="https://ja.wikipedia.org/wiki/&#x5b97;&#x6559;&#x4eba;&#x985e;&#x5b66;" MODIFIED="1605167195929" TEXT="d.1. &#x5b97;&#x6559;&#x4eba;&#x985e;&#x5b66; (wi)">
+<node CREATED="1605166701036" ID="ID_1657397259" LINK="https://ja.wikipedia.org/wiki/&#x7d44;&#x7e54;_(&#x793e;&#x4f1a;&#x79d1;&#x5b66;)" MODIFIED="1605166710865" TEXT="1. &#x7d44;&#x7e54;_(&#x793e;&#x4f1a;&#x79d1;&#x5b66;)"/>
+<node CREATED="1605166913422" ID="ID_223753293" MODIFIED="1605166915378" TEXT="fur">
+<node CREATED="1605166916143" ID="ID_1698654059" MODIFIED="1605166924921" TEXT="1. &#x30d3;&#x30fc;&#x30eb;&#x30fc;&#x30cb;&#x30fc;">
+<arrowlink DESTINATION="ID_1933690052" ENDARROW="Default" ENDINCLINATION="349;0;" ID="Arrow_ID_938900015" STARTARROW="None" STARTINCLINATION="349;0;"/>
+<node CREATED="1605167165557" ID="ID_821645928" MODIFIED="1605167167850" TEXT="docs">
+<node CREATED="1605167168757" ID="ID_1748936320" LINK="https://ja.wikipedia.org/wiki/&#x30d3;&#x30fc;&#x30eb;&#x30fc;&#x30cb;&#x30fc;" MODIFIED="1605167233617" TEXT="d.2-1. &#x30d3;&#x30fc;&#x30eb;&#x30fc;&#x30cb;&#x30fc; (wi)">
+<node CREATED="1605168049548" ID="ID_560090906" MODIFIED="1605168052786" TEXT="langs">
+<node CREATED="1605167686854" ID="ID_21788457" LINK="https://ko.wikipedia.org/wiki/&#xbe44;&#xb8e8;&#xb2c8;" MODIFIED="1605167703578" TEXT="d.2-1.L-1 &#xbe44;&#xb8e8;&#xb2c8; (wi)"/>
+<node CREATED="1605168020485" ID="ID_777250653" LINK="https://pl.wikipedia.org/wiki/Biruni" MODIFIED="1605168037810" TEXT="d.2-1.L-2 Biruni (wi)">
+<node CREATED="1605168099437" ID="ID_898388462" MODIFIED="1605168100417" TEXT="texts">
+<node COLOR="#338800" CREATED="1605168101261" ID="ID_1557716606" MODIFIED="1605168107181" TEXT="#1"/>
+<node COLOR="#338800" CREATED="1605168102277" ID="ID_1745299063" MODIFIED="1605168453913" TEXT="#2 W pracach z dziedziny">
+<node CREATED="1605168448925" ID="ID_629737991" MODIFIED="1605168451818" TEXT="~ drogocennych kamieni i metali"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1605166937965" ID="ID_37501142" MODIFIED="1605166940217" TEXT="2. &#x30a4;&#x30d6;&#x30f3;&#xff1d;&#x30cf;&#x30eb;&#x30c9;&#x30a5;&#x30fc;&#x30f3;"/>
+<node CREATED="1605166949453" ID="ID_1563282269" MODIFIED="1605166951089" TEXT="3. &#x30eb;&#x30fc;&#x30c8;&#x30f4;&#x30a3;&#x30d2;&#x30fb;&#x30d5;&#x30a9;&#x30a4;&#x30a8;&#x30eb;&#x30d0;&#x30c3;&#x30cf;"/>
+</node>
+<node CREATED="1605168620558" ID="ID_490807555" MODIFIED="1605168621545" TEXT="langs">
+<node CREATED="1605168633893" ID="ID_765402798" LINK="https://zh.wikipedia.org/zh-cn/&#x5b97;&#x6559;&#x4eba;&#x7c7b;&#x5b66;" MODIFIED="1605168653435" TEXT="d.1.L-1 &#x5b97;&#x6559;&#x4eba;&#x7c7b;&#x5b66; (wiki)">
+<node CREATED="1605169101484" ID="ID_1183044019" MODIFIED="1605169102595" TEXT="texts">
+<node CREATED="1605169103277" ID="ID_63064093" MODIFIED="1605169104145" TEXT="#1"/>
+<node CREATED="1605169104604" ID="ID_586545940" LINK="https://zh.wikipedia.org/zh-cn/&#x5b97;&#x6559;&#x4eba;&#x7c7b;&#x5b66;#&#x5b97;&#x6559;&#x7684;&#x5b9a;&#x7fa9;" MODIFIED="1605169112906" TEXT="#2 &#x5b97;&#x6559;&#x7684;&#x5b9a;&#x7fa9;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
