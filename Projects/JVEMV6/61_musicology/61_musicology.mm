d0b499ae6df7ff32e86b68d407faec7e47b445d6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-28 01:19:29 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/61_musicology/61_musicology.mm
+++ Projects/JVEMV6/61_musicology/61_musicology.mm
@@ -1,4 +1,20 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1605364966551" ID="ID_1334383853" LINK="../../JVEMV6.mm" MODIFIED="1605366431336" TEXT="61. musicology"/>
+<node CREATED="1605364966551" ID="ID_1334383853" LINK="../../JVEMV6.mm" MODIFIED="1605366431336" TEXT="61. musicology">
+<node CREATED="1606464218010" ID="ID_261912613" MODIFIED="1606464222171" POSITION="right" TEXT="1. music theory">
+<node CREATED="1606464228058" ID="ID_451949468" MODIFIED="1606464240510" TEXT="topics">
+<node CREATED="1606464241257" ID="ID_1702603476" MODIFIED="1606464248815" TEXT="1. japanese music thoeries">
+<node CREATED="1606464282118" ID="ID_580641866" MODIFIED="1606464284163" TEXT="search">
+<node CREATED="1606464276854" ID="ID_1703543133" LINK="https://duckduckgo.com/?q=&#x65e5;&#x672c;+&#x97f3;&#x697d;&#x7406;&#x8ad6;&amp;t=opera&amp;ia=web&amp;iai=r1-3&amp;page=1&amp;sexp={&quot;nctexp&quot;:&quot;b&quot;,&quot;biaexp&quot;:&quot;b&quot;,&quot;msvrtexp&quot;:&quot;b&quot;}" MODIFIED="1606464301119" TEXT="1. q=&#x65e5;&#x672c;+&#x97f3;&#x697d;&#x7406;&#x8ad6;"/>
+</node>
+<node CREATED="1606464274440" ID="ID_1584252238" MODIFIED="1606464275813" TEXT="docs"/>
+</node>
+</node>
+</node>
+<node CREATED="1606466459265" ID="ID_980547566" MODIFIED="1606466465587" POSITION="right" TEXT="2. korean traditional music">
+<node CREATED="1606466483203" ID="ID_593587271" MODIFIED="1606466489877" TEXT="1. topics">
+<node CREATED="1606466490692" ID="ID_1595654513" LINK="https://ko.wikipedia.org/wiki/&#xc0b0;&#xc870;" MODIFIED="1606466502197" TEXT="1. &#xc0b0;&#xc870;"/>
+</node>
+</node>
+</node>
 </map>
