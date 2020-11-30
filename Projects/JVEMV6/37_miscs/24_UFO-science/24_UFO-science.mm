94e8c6b6bcff5f30c3427062d53f9726781e08d3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-11-30 17:58:28 +0900

    (FM_2_20171104_225946) periodical updates

---
+++ Projects/JVEMV6/37_miscs/24_UFO-science/24_UFO-science.mm
@@ -0,0 +1,21 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1587306289165" ID="ID_1566300838" LINK="../2)_37_miscs-of-miscs.mm" MODIFIED="1606722413606" TEXT="24 UFO science">
+<node CREATED="1606722437250" ID="ID_606678698" MODIFIED="1606722438570" POSITION="right" TEXT="1. eman:relativity"/>
+<node CREATED="1606722423261" ID="ID_351946156" MODIFIED="1606722424135" POSITION="right" TEXT="5. doc:M-9"/>
+<node CREATED="1606722452732" ID="ID_321308612" MODIFIED="1606722453374" POSITION="right" TEXT="6. physics"/>
+<node CREATED="1606724133879" ID="ID_847892551" MODIFIED="1606724146095" POSITION="right" TEXT="7. relativity_anoda">
+<node CREATED="1606724148028" ID="ID_1297028129" MODIFIED="1606724148950" TEXT="docs">
+<node CREATED="1606724184182" ID="ID_991405497" LINK="http://anoda.web.fc2.com/oldpage/space/mlab07/mlab07.htm" MODIFIED="1606724186875" TEXT="1."/>
+</node>
+</node>
+<node CREATED="1606724343055" ID="ID_969960591" MODIFIED="1606724359070" POSITION="right" TEXT="8. ufo engineering">
+<node CREATED="1606724360012" ID="ID_1527401553" MODIFIED="1606724361683" TEXT="serarch">
+<node CREATED="1606724362391" ID="ID_685963845" LINK="https://3g2upl4pq6kufc4m.onion/?q=ufo+engineering+site:ufo-engineering.com&amp;ia=web" MODIFIED="1606724411622" TEXT="1. q=ufo+engineering+site:ufo-engineering.com"/>
+</node>
+<node CREATED="1606724414085" ID="ID_1502375299" MODIFIED="1606724415054" TEXT="docs">
+<node CREATED="1606724435168" ID="ID_982485946" LINK="http://viewzone.com/ufopropulsionx.html" MODIFIED="1606724877432" TEXT="d.8-1. ufopropulsion"/>
+</node>
+</node>
+</node>
+</map>
