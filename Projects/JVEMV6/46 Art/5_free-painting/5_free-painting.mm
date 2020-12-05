74cade321e5b1bf1b8e0d0fd7d8896610180eed0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-05 17:21:12 +0900

    (FM_2_20171104_225946) periodical updates

--- Projects/JVEMV6/46 Art/5_free-painting/5_free-painting.mm
+++ Projects/JVEMV6/46 Art/5_free-painting/5_free-painting.mm
@@ -2,6 +2,10 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1542548710067" ID="ID_405026175" LINK="../../../46%20Art.6%20Visual%20Arts.mm" MODIFIED="1606617042207" TEXT="5 free painting">
 <node CREATED="1606617047353" ID="ID_769162771" MODIFIED="1606617587928" POSITION="right" TEXT="1. miscs"/>
-<node CREATED="1606617589201" ID="ID_1948327036" MODIFIED="1606617597142" POSITION="right" TEXT="2. &#x98a8;&#x795e;"/>
+<node CREATED="1606617589201" ID="ID_1948327036" MODIFIED="1606617597142" POSITION="right" TEXT="2. &#x98a8;&#x795e;">
+<node CREATED="1607155528084" ID="ID_1987106001" MODIFIED="1607155529863" TEXT="sessions">
+<node CREATED="1607155530618" ID="ID_422052930" MODIFIED="1607155531009" TEXT="2"/>
+</node>
+</node>
 </node>
 </map>
