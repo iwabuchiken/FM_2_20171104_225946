06ae7da50d4d9cebfff88ba4945957b37b2d8a19
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-29 12:47:23 +0900

    (JVE_67.[theme=theology]) periodical updates

---
+++ Projects/JVEMV6/65_philosophy/4_japanese-history/4_japanese-history.mm
@@ -0,0 +1,61 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1604219829243" ID="ID_611530253" LINK="../65_philosophy.mm" MODIFIED="1609212746614" TEXT="4. japanese history">
+<node CREATED="1604219840218" ID="ID_1087520442" MODIFIED="1609212586787" POSITION="right" TEXT="1. 2nd-world-war">
+<node CREATED="1604219867104" ID="ID_324305582" MODIFIED="1609212587286" TEXT="1. &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;">
+<node CREATED="1604219887107" ID="ID_1788330577" MODIFIED="1609212587832" TEXT="docs">
+<node CREATED="1604220053755" FOLDED="true" ID="ID_965239931" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;" MODIFIED="1609212572623" TEXT="1. &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044; (wi)">
+<node CREATED="1604220101868" FOLDED="true" ID="ID_1259988080" MODIFIED="1609212572623" TEXT="texts">
+<node COLOR="#338800" CREATED="1604220056096" MODIFIED="1604221541001" TEXT="#1"/>
+<node COLOR="#338800" CREATED="1604220059580" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x80cc;&#x666f;" MODIFIED="1604221541002" TEXT="#2 &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x80cc;&#x666f;"/>
+<node COLOR="#338800" CREATED="1604221311181" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x6226;&#x95d8;&#x306e;&#x7d4c;&#x904e;" MODIFIED="1604221541002" TEXT="#3 &#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x6226;&#x95d8;&#x306e;&#x7d4c;&#x904e;"/>
+<node COLOR="#338800" CREATED="1604220713435" MODIFIED="1604389865306" TEXT="#4 &#x4e0a;&#x9678;&#x5f53;&#x521d;&#x3001;&#x6700;&#x3082;&#x6575;&#x306b;"/>
+<node COLOR="#338800" CREATED="1604298705479" ID="ID_693092181" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x7b2c;&#x4e00;&#x6b21;&#x30bd;&#x30ed;&#x30e2;&#x30f3;&#x6d77;&#x6226;" MODIFIED="1604390411325" TEXT="#5 &#x7b2c;&#x4e00;&#x6b21;&#x30bd;&#x30ed;&#x30e2;&#x30f3;&#x6d77;&#x6226;"/>
+<node COLOR="#338800" CREATED="1604390403987" MODIFIED="1604390531844" TEXT="#6 25&#x822a;&#x6226;&#x306f;34&#x6a5f;&#x55aa;&#x5931;&#x306e;"/>
+<node COLOR="#338800" CREATED="1604390527445" FOLDED="true" ID="ID_1562491111" MODIFIED="1609212572623" TEXT="#7 &#x4e09;&#x5ddd;&#x4e2d;&#x5c06;&#x7387;&#x3044;&#x308b;">
+<node CREATED="1604390796172" FOLDED="true" ID="ID_1339224540" MODIFIED="1609212572623" TEXT="fur">
+<node CREATED="1604390799893" ID="ID_946536919" MODIFIED="1604390925412" TEXT="topic-1. &#x30e9;&#x30d0;&#x30a6;&#x30eb;"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1604390714564" ID="ID_1958394564" MODIFIED="1604391871259" TEXT="#8 &#x4e0a;&#x9678;&#x521d;&#x65e5;&#x304b;&#x3089;&#x306e;"/>
+<node COLOR="#338800" CREATED="1604391866790" ID="ID_679838895" MODIFIED="1604542057647" TEXT="#9 18&#x65e5;&#x3001;&#x540c;&#x5ddd;&#x897f;&#x5cb8;&#x306e;&#x65e5;&#x672c;&#x8ecd;&#x3092;"/>
+<node COLOR="#338800" CREATED="1604542048731" ID="ID_1366742773" LINK="https://ja.wikipedia.org/wiki/&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;&#x306e;&#x6226;&#x3044;#&#x30a4;&#x30eb;&#x5ddd;&#x6e21;&#x6cb3;&#x6226;" MODIFIED="1604543244097" TEXT="#10 &#x30a4;&#x30eb;&#x5ddd;&#x6e21;&#x6cb3;&#x6226;"/>
+<node CREATED="1604543239144" FOLDED="true" ID="ID_1968737936" MODIFIED="1609212572623" TEXT="#11 8&#x6708;18&#x65e5;&#x306b;&#x30ac;&#x30c0;&#x30eb;&#x30ab;&#x30ca;&#x30eb;&#x5cf6;">
+<node COLOR="#3333ff" CREATED="1603356800076" ID="ID_431729473" MODIFIED="1604543912318" TEXT="@@@">
+<cloud COLOR="#ffff66"/>
+<font NAME="SansSerif" SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1604220793028" ID="ID_386276054" LINK="https://en.wikipedia.org/wiki/Guadalcanal_campaign#/media/File:Guadalcanal_Aug_7_landings.svg" MODIFIED="1604220812000" TEXT="1.1. Guadalcanal_Aug_7_landings.svg"/>
+<node CREATED="1604221090484" LINK="https://en.wikipedia.org/wiki/Ironbottom_Sound" MODIFIED="1604221098285" TEXT="1.2. Ironbottom_Sound"/>
+</node>
+</node>
+</node>
+<node CREATED="1609212595341" ID="ID_1083680708" MODIFIED="1609212599555" POSITION="right" TEXT="2. &#x4e00;&#x63c6;">
+<node CREATED="1609212603032" ID="ID_1445132631" MODIFIED="1609212606084" TEXT="topics"/>
+<node CREATED="1609212650134" ID="ID_1196417110" MODIFIED="1609212651415" TEXT="docs">
+<node CREATED="1609212651998" ID="ID_1576298076" LINK="https://ja.wikipedia.org/wiki/Category:&#x4e00;&#x63c6;" MODIFIED="1609212711763" TEXT="2-d-1. Category:&#x4e00;&#x63c6; wi"/>
+</node>
+<node CREATED="1609212600715" ID="ID_788019543" MODIFIED="1609212602246" TEXT="sessions"/>
+</node>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1608627705665" POSITION="left" TEXT="git">
+<node CREATED="1608627706371" FOLDED="true" ID="ID_492644994" MODIFIED="1609212788241" TEXT="1">
+<node CREATED="1608627706941" ID="ID_1183827369" MODIFIED="1609212783579">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_65.[theme=philosophy].[4.japanese-history]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
--- Projects/JVEMV6/65_philosophy/65_philosophy.mm
+++ Projects/JVEMV6/65_philosophy/65_philosophy.mm
@@ -4,7 +4,7 @@
 <node CREATED="1577864425389" ID="ID_1718757547" LINK="../../37_A1_&#x4ecf;&#x6559;.mm" MODIFIED="1604565512549" POSITION="right" TEXT="A1 &#x4ecf;&#x6559;"/>
 <node CREATED="1585546284584" ID="ID_25244323" MODIFIED="1585546291412" POSITION="right" TEXT="2 &#x30e1;&#x30bf;&#x502b;&#x7406;&#x5b66;"/>
 <node CREATED="1605080825316" ID="ID_1555102210" LINK="3_afro-american_peoples-history/3_afro-american_peoples-history.mm" MODIFIED="1605080825347" POSITION="right" TEXT="3 afro-american people&apos;s history"/>
-<node CREATED="1604390901349" ID="ID_488933966" LINK="4_japanese-history/4_japanese-history.mm" MODIFIED="1604390901349" POSITION="right" TEXT="4. japanese history"/>
+<node CREATED="1604390901349" ID="ID_488933966" LINK="4_japanese-history/4_japanese-history.mm" MODIFIED="1609212566956" POSITION="right" TEXT="4. japanese history"/>
 <node CREATED="1604565721368" ID="ID_149497451" LINK="../37_miscs/20_philosophy/5_analytic-philosophy.mm" MODIFIED="1607916798701" POSITION="right" TEXT="5. &#x5206;&#x6790;&#x54f2;&#x5b66;"/>
 <node CREATED="1604813187937" ID="ID_849756492" LINK="6_social-issues/6_social-issues.mm" MODIFIED="1604813187942" POSITION="right" TEXT="6. social issues">
 <node CREATED="1606747112512" ID="ID_642770300" LINK="../../63_social-issues/63_social-issues.mm" MODIFIED="1606747261711" TEXT="==&gt; move to JVE 63"/>
