d1ab22e27ea2ff5229d6b47d41b49c7a40f7d802
iwabuchiken <iwabuchi.k.2010@gmail.com>
2020-12-31 14:06:51 +0900

    (JVE_65.[theme=philosophy].[9.jamaal]) periodical updates

---
+++ Projects/JVEMV6/65_philosophy/9_jamaal/9_jamaal.mm
@@ -0,0 +1,76 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1609388104720" ID="ID_369052362" LINK="../65_philosophy.mm" MODIFIED="1609388439708" TEXT="9. &#x7f8e;">
+<node CREATED="1609389988710" ID="ID_1365330380" MODIFIED="1609389994592" POSITION="right" TEXT="genres">
+<node CREATED="1609389995373" ID="ID_1580581535" MODIFIED="1609390089050" TEXT="A-1">
+<node CREATED="1609389996655" ID="ID_161699749" MODIFIED="1609389998706" TEXT="general"/>
+</node>
+</node>
+<node CREATED="1609388462966" ID="ID_962336223" MODIFIED="1609388465054" POSITION="right" TEXT="sessions">
+<node COLOR="#338800" CREATED="1609388473288" FOLDED="true" ID="ID_34468996" MODIFIED="1609391147853" TEXT="20201231_132112">
+<node COLOR="#338800" CREATED="1609388474614" FOLDED="true" ID="ID_1482813886" MODIFIED="1609391147852" TEXT="1">
+<node CREATED="1609388476094" FOLDED="true" ID="ID_1900348933" MODIFIED="1609391147851" TEXT="read">
+<node CREATED="1609388514028" FOLDED="true" ID="ID_901883836" MODIFIED="1609391147851" TEXT="docs">
+<node CREATED="1609388515592" FOLDED="true" ID="ID_1527290204" LINK="https://ja.wikipedia.org/wiki/&#x7f8e;&#x5b66;" MODIFIED="1609391147849" TEXT="A-1.d-1. &#x7f8e;&#x5b66; wi">
+<node CREATED="1609389003377" FOLDED="true" ID="ID_841517107" LINK="https://ko.wikipedia.org/wiki/&#xbbf8;&#xd559;" MODIFIED="1609391147847" TEXT="A-1.d-1.L-1 &#xbbf8;&#xd559; wi">
+<node CREATED="1609389797406" ID="ID_423509298" MODIFIED="1609389841329" TEXT="fur"/>
+<node CREATED="1609389799789" FOLDED="true" ID="ID_1889581097" MODIFIED="1609391147847" TEXT="search">
+<node CREATED="1609389812336" ID="ID_1677883936" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#xbd84;&#xc11d;&#xbbf8;&#xd559;&amp;ia=web" MODIFIED="1609389824498" TEXT="1. q=&#xbd84;&#xc11d;&#xbbf8;&#xd559;"/>
+</node>
+</node>
+</node>
+<node CREATED="1609388553549" ID="ID_948199457" LINK="https://ja.wikipedia.org/wiki/Category:&#x5bb9;&#x59ff;" MODIFIED="1609390111605" TEXT="A-1.d-1.1 Category:&#x5bb9;&#x59ff;"/>
+<node CREATED="1609388582650" ID="ID_1952886655" LINK="https://ja.wikipedia.org/wiki/&#x7f8e;&#x4eba;" MODIFIED="1609390111606" TEXT="A-1.d-1.2 &#x7f8e;&#x4eba; wi"/>
+<node CREATED="1609388610105" ID="ID_664930318" LINK="https://ja.wikipedia.org/wiki/&#x7f8e;" MODIFIED="1609390111607" TEXT="A-1.d-1.3 &#x7f8e; wi"/>
+<node CREATED="1609388650814" ID="ID_1051186134" LINK="https://ja.wikipedia.org/wiki/Category:&#x7f8e;&#x5b66;&#x306e;&#x6982;&#x5ff5;" MODIFIED="1609390111608" TEXT="A-1.d-1.4 Category:&#x7f8e;&#x5b66;&#x306e;&#x6982;&#x5ff5;"/>
+<node CREATED="1609388671466" ID="ID_1387110533" LINK="https://ja.wikipedia.org/wiki/&#x3084;&#x3064;&#x3057;" MODIFIED="1609390111609" TEXT="A-1.d-1.5 &#x3084;&#x3064;&#x3057;"/>
+<node CREATED="1609388695965" FOLDED="true" ID="ID_473640654" LINK="https://ja.wikipedia.org/wiki/&#x898b;&#x7acb;&#x3066;" MODIFIED="1609391147850" TEXT="A-1.d-1.6 &#x898b;&#x7acb;&#x3066; wi">
+<node CREATED="1609389867406" ID="ID_1737400666" MODIFIED="1609389868578" TEXT="texts"/>
+<node CREATED="1609389964317" FOLDED="true" ID="ID_615444856" MODIFIED="1609391147850" TEXT="fur">
+<node CREATED="1609389966421" ID="ID_1164333239" LINK="https://ja.wikipedia.org/wiki/&#x898b;&#x7acb;&#x7d75;" MODIFIED="1609390120723" TEXT="A-1.d-2. &#x898b;&#x7acb;&#x7d75; wi"/>
+</node>
+</node>
+<node CREATED="1609388722087" ID="ID_1744292708" LINK="https://ja.wikipedia.org/wiki/Category:&#x54f2;&#x5b66;&#x306e;&#x6982;&#x5ff5;" MODIFIED="1609390111611" TEXT="A-1.d-1.7 Category:&#x54f2;&#x5b66;&#x306e;&#x6982;&#x5ff5; wi"/>
+</node>
+</node>
+</node>
+<node CREATED="1609391037886" FOLDED="true" ID="ID_1394689635" MODIFIED="1609391147852" TEXT="n:">
+<node CREATED="1609391040861" ID="ID_1196516386" MODIFIED="1609391141693">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#20170;&#22238;&#12392;&#21516;&#27096;&#12395;&#12289;&#65297;&#12388;&#12363;&#65298;&#12388;&#12289;&#12473;&#12479;&#12540;&#12488;&#38917;&#30446;&#12434;&#36984;&#12406;&#12290;&#12288;
+    </p>
+    <p>
+      &#12288;&#12288;=&gt;&#12288;&quot;&#38917;&#30446;&#12383;&#12368;&#12426;&#25506;&#32034;&quot;&#12289;&#36914;&#12417;&#12424;&#12358;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1609391031950" ID="ID_545724993" MODIFIED="1609391034543" TEXT="/20201231_140348"/>
+</node>
+</node>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1608627705665" POSITION="left" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1608627706940" TEXT="1">
+<node CREATED="1608627706941" ID="ID_1183827369" MODIFIED="1609391185283">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_65.[theme=philosophy].[9.jamaal]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
