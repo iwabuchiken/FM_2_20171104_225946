5d73eb20e319858b86f50c3f8c09ba611224360f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-01-06 09:09:11 +0900

    (JVE_61.[theme=musicology]) periodical updates

--- Projects/JVEMV6/61_musicology/61_musicology.mm
+++ Projects/JVEMV6/61_musicology/61_musicology.mm
@@ -1,7 +1,8 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1605364966551" ID="ID_1334383853" LINK="../../JVEMV6.mm" MODIFIED="1605366431336" TEXT="61. musicology">
-<node CREATED="1606464218010" ID="ID_261912613" MODIFIED="1606464222171" POSITION="right" TEXT="1. music theory">
+<node CREATED="1609891613062" ID="ID_113878831" MODIFIED="1609891626350" POSITION="right" TEXT="areas">
+<node CREATED="1606464218010" ID="ID_261912613" MODIFIED="1609891632989" TEXT="A-1. music theory">
 <node CREATED="1606464228058" ID="ID_451949468" MODIFIED="1606464240510" TEXT="topics">
 <node CREATED="1606464241257" ID="ID_1702603476" MODIFIED="1606464248815" TEXT="1. japanese music thoeries">
 <node CREATED="1606464282118" ID="ID_580641866" MODIFIED="1606464284163" TEXT="search">
@@ -11,9 +12,56 @@
 </node>
 </node>
 </node>
-<node CREATED="1606466459265" ID="ID_980547566" MODIFIED="1606466465587" POSITION="right" TEXT="2. korean traditional music">
+<node CREATED="1606466459265" ID="ID_980547566" MODIFIED="1609891635973" TEXT="A-2. korean traditional music">
 <node CREATED="1606466483203" ID="ID_593587271" MODIFIED="1606466489877" TEXT="1. topics">
 <node CREATED="1606466490692" ID="ID_1595654513" LINK="https://ko.wikipedia.org/wiki/&#xc0b0;&#xc870;" MODIFIED="1606466502197" TEXT="1. &#xc0b0;&#xc870;"/>
+</node>
+</node>
+</node>
+<node CREATED="1609891609717" ID="ID_224444133" MODIFIED="1609891612278" POSITION="right" TEXT="sessions">
+<node COLOR="#338800" CREATED="1609891639688" ID="ID_453949697" MODIFIED="1609891695936" TEXT="s-1">
+<node CREATED="1609891658153" ID="ID_1048792991" MODIFIED="1609891658153" TEXT="20210106_090735"/>
+<node CREATED="1609891660904" ID="ID_1639984654" MODIFIED="1609891661364" TEXT="1">
+<node CREATED="1609891661784" ID="ID_1672875589" MODIFIED="1609891664797" TEXT="compo">
+<node CREATED="1609891666031" ID="ID_1144152629" MODIFIED="1609891668052" TEXT="R=1"/>
+</node>
+</node>
+<node CREATED="1609891690280" ID="ID_1781648242" MODIFIED="1609891691733" TEXT="n:">
+<node CREATED="1609891692312" ID="ID_678365351" MODIFIED="1609891693347" TEXT="any"/>
+</node>
+</node>
+</node>
+<node CREATED="1609453656833" ID="ID_37527138" MODIFIED="1609453658503" POSITION="left" TEXT="utils">
+<node CREATED="1609426935847" ID="ID_677494908" LINK="C:\WORKS_2\t.bat" MODIFIED="1609426941038" TEXT="time"/>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1608627705665" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1608627706940" TEXT="1">
+<node CREATED="1608627706941" ID="ID_1183827369" MODIFIED="1609891722405">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_61.[theme=musicology]) periodical updates&quot; &amp;&amp; e &amp;&amp; p &amp;&amp; r oj &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_61.[theme=musicology]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_1905849771" MODIFIED="1609891737623">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_61.[theme=musicology]) periodical updates&quot; &amp;&amp; e &amp;&amp; p &amp;&amp; r oj &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_61.[theme=musicology]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </node>
