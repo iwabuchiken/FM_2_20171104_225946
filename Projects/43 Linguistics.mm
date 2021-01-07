91d1230fbeb0296f9155deaa04a48d622f5f65a7
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-01-07 11:26:11 +0900

    (JVE_43.[theme=Linguistics]) periodical updates

--- Projects/43 Linguistics.mm
+++ Projects/43 Linguistics.mm
@@ -1,11 +1,13 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1517805550393" ID="ID_709290637" LINK="Projects.mm" MODIFIED="1517826557550" TEXT="43 Linguistics">
-<node CREATED="1517812528128" ID="ID_939601191" MODIFIED="1517812530413" POSITION="right" TEXT="1) Topics">
-<node CREATED="1517812531362" ID="ID_1122011499" LINK="https://ja.wikipedia.org/wiki/&#x81a0;&#x7740;&#x8a9e;" MODIFIED="1517825937094" TEXT="1-1) &#x81a0;&#x7740;&#x8a9e;">
+<node CREATED="1517805550393" ID="ID_709290637" LINK="Projects.mm" MODIFIED="1609977771367" TEXT="43 Linguistics">
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<node CREATED="1517812528128" FOLDED="true" ID="ID_939601191" MODIFIED="1609986264455" POSITION="right" TEXT="1) Topics">
+<node CREATED="1609977808174" FOLDED="true" ID="ID_361286615" MODIFIED="1609986258308" TEXT="A-1">
+<node CREATED="1517812531362" FOLDED="true" ID="ID_1122011499" LINK="https://ja.wikipedia.org/wiki/&#x81a0;&#x7740;&#x8a9e;" MODIFIED="1609986258304" TEXT="1-1) &#x81a0;&#x7740;&#x8a9e;">
 <node CREATED="1517825994901" ID="ID_895073450" LINK="https://ja.wikipedia.org/wiki/&#x30b3;&#x30d4;&#x30e5;&#x30e9;" MODIFIED="1517826016042" TEXT="1-1-1) &#x30b3;&#x30d4;&#x30e5;&#x30e9;"/>
 <node CREATED="1517826079983" ID="ID_95060924" LINK="https://ja.wikipedia.org/wiki/&#x30a6;&#x30e9;&#x30eb;&#x8a9e;&#x65cf;" MODIFIED="1517826092057" TEXT="1-1-2) &#x30a6;&#x30e9;&#x30eb;&#x8a9e;&#x65cf;"/>
-<node CREATED="1517826183408" ID="ID_1514337355" LINK="https://ja.wikipedia.org/wiki/&#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8af8;&#x8a9e;" MODIFIED="1517826195933" TEXT="1-1-3) &#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8af8;&#x8a9e;">
+<node CREATED="1517826183408" FOLDED="true" ID="ID_1514337355" LINK="https://ja.wikipedia.org/wiki/&#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8af8;&#x8a9e;" MODIFIED="1609986258302" TEXT="1-1-3) &#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8af8;&#x8a9e;">
 <node CREATED="1517826301142" ID="ID_1630761142" LINK="https://ja.wikipedia.org/wiki/&#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8af8;&#x8a9e;#&#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8a9e;&#x65cf;" MODIFIED="1517826893124" TEXT="1-1-3-1) &#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x8a9e;&#x65cf;">
 <linktarget COLOR="#b0b0b0" DESTINATION="ID_1630761142" ENDARROW="Default" ENDINCLINATION="54;64;" ID="Arrow_ID_1545219854" SOURCE="ID_876266854" STARTARROW="None" STARTINCLINATION="128;22;"/>
 </node>
@@ -14,15 +16,100 @@
 <node CREATED="1517826877818" ID="ID_876266854" LINK="https://ja.wikipedia.org/wiki/&#x6bcd;&#x97f3;&#x8abf;&#x548c;" MODIFIED="1517826893119" TEXT="1-2) &#x6bcd;&#x97f3;&#x8abf;&#x548c;">
 <arrowlink DESTINATION="ID_1630761142" ENDARROW="Default" ENDINCLINATION="54;64;" ID="Arrow_ID_1545219854" STARTARROW="None" STARTINCLINATION="128;22;"/>
 </node>
-<node CREATED="1517832271407" ID="ID_783132570" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;" MODIFIED="1517832290939" TEXT="1-3) &#x7d71;&#x8a9e;&#x8ad6;">
+<node CREATED="1517832271407" FOLDED="true" ID="ID_783132570" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;" MODIFIED="1609986258307" TEXT="1-3) &#x7d71;&#x8a9e;&#x8ad6;">
 <node CREATED="1517832275375" ID="ID_1218325275" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;" MODIFIED="1517832349992" TEXT="1-3-1) &#x8a9e;"/>
-<node CREATED="1517832277417" ID="ID_1887953098" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;" MODIFIED="1517832352015" TEXT="1-3-2) &#x53e5;">
+<node CREATED="1517832277417" FOLDED="true" ID="ID_1887953098" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;" MODIFIED="1609986258305" TEXT="1-3-2) &#x53e5;">
 <node CREATED="1517832317087" ID="ID_893245748" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;#&#x4e3b;&#x8981;&#x90e8;&#x3068;&#x5f93;&#x5c5e;&#x90e8;" MODIFIED="1517832330626" TEXT="1-3-2-1) &#x4e3b;&#x8981;&#x90e8;"/>
 <node CREATED="1517832331396" ID="ID_1527284001" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x8a9e;&#x8ad6;#&#x4e3b;&#x8981;&#x90e8;&#x3068;&#x5f93;&#x5c5e;&#x90e8;" MODIFIED="1517832340981" TEXT="1-3-2-2) &#x5f93;&#x5c5e;&#x90e8;"/>
 </node>
 </node>
 </node>
+<node CREATED="1609977816528" FOLDED="true" ID="ID_1107554077" MODIFIED="1609986258312" TEXT="A-2 / general">
+<node CREATED="1609977830949" FOLDED="true" ID="ID_1594973233" MODIFIED="1609986258311" TEXT="docs">
+<node CREATED="1609977846637" FOLDED="true" ID="ID_1618615480" LINK="https://ko.wikipedia.org/wiki/&#xc5b8;&#xc5b4;&#xd559;" MODIFIED="1609986258310" TEXT="A-2.d-1 / &#xc5b8;&#xc5b4;&#xd559; wi">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1618615480" ENDARROW="Default" ENDINCLINATION="165;0;" ID="Arrow_ID_323952835" SOURCE="ID_1704779739" STARTARROW="None" STARTINCLINATION="165;0;"/>
+<node CREATED="1609977912574" FOLDED="true" ID="ID_208858764" MODIFIED="1609986258309" TEXT="texts">
+<node COLOR="#990000" CREATED="1609977961422" ID="ID_20280367" MODIFIED="1609977964450" TEXT="20210107_090556/"/>
+<node COLOR="#338800" CREATED="1609977916224" ID="ID_394735758" MODIFIED="1609977940340" TEXT="#1"/>
+<node COLOR="#338800" CREATED="1609977937305" ID="ID_817009319" MODIFIED="1609980190450" TEXT="#2 &#xc704;&#xc5d0;&#xc11c; &#xd615;&#xd0dc;&#xb860;&#xacfc;"/>
+<node COLOR="#338800" CREATED="1609980187368" ID="ID_855773409" MODIFIED="1609980496757" TEXT="#3 &#xc5b4;&#xb5a0;&#xd55c; &#xcc28;&#xc6d0;&#xc5d0;&#xc11c; &#xc5b8;&#xc5b4;&#xb97c; "/>
+<node CREATED="1609980493800" ID="ID_335134382" MODIFIED="1609980495752" TEXT="#4 &#xb610;&#xd55c; &#xc5b8;&#xc5b4;&#xc758; &#xc5b4;&#xb5a4; &#xce21;&#xba74;&#xc744;"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1609980627050" FOLDED="true" ID="ID_1078227063" MODIFIED="1609986264454" TEXT="A-3 / various">
+<node CREATED="1609980633001" FOLDED="true" ID="ID_994249777" LINK="https://ko.wikipedia.org/wiki/&#xba54;&#xd0c0;_&#xc5b8;&#xc5b4;" MODIFIED="1609986258314" TEXT="&#xba54;&#xd0c0;_&#xc5b8;&#xc5b4; wi">
+<node CREATED="1609980939390" FOLDED="true" ID="ID_1021400936" MODIFIED="1609986258313" TEXT="fur">
+<node CREATED="1609980946346" ID="ID_1866799312" LINK="https://blog.aladin.co.kr/Pansees/6632806" MODIFIED="1609980956175" TEXT="&#xba54;&#xd0c0;&#xc5b8;&#xc5b4; (meta&#x8a00;&#x8a9e;)&#xb780;?"/>
+</node>
+</node>
+<node CREATED="1609981649255" ID="ID_592289850" LINK="https://zh.wikipedia.org/zh-cn/&#x81ea;&#x6211;&#x6b78;&#x985e;&#x7406;&#x8ad6;" MODIFIED="1609981655021" TEXT="&#x81ea;&#x6211;&#x6b78;&#x985e;&#x7406;&#x8ad6; wi:cn"/>
+<node CREATED="1609981790095" ID="ID_889172662" LINK="https://zh.wikipedia.org/wiki/Category:&#x7b26;&#x865f;&#x5b78;" MODIFIED="1609981804275" TEXT="Category:&#x7b26;&#x865f;&#x5b78;"/>
+<node CREATED="1609981965635" ID="ID_1994244450" LINK="https://ja.wikipedia.org/wiki/&#x7d71;&#x5236;&#x8a9e;&#x5f59;" MODIFIED="1609981976830" TEXT="&#x7d71;&#x5236;&#x8a9e;&#x5f59; wi"/>
+<node CREATED="1609982001574" ID="ID_809621365" LINK="https://zh.wikipedia.org/wiki/Category:&#x6027;&#x59ff;&#x52e2;" MODIFIED="1609982006952" TEXT="Category:&#x6027;&#x59ff;&#x52e2;"/>
+<node CREATED="1609982023963" ID="ID_705608991" LINK="https://zh.wikipedia.org/wiki/Category:&#x6587;&#x5316;" MODIFIED="1609982034050" TEXT="Category:&#x6587;&#x5316;"/>
+<node CREATED="1609982616719" ID="ID_1877563094" LINK="https://zh.wikipedia.org/wiki/Category:&#x8a9e;&#x7528;&#x5b78;" MODIFIED="1609982621530" TEXT="Category:&#x8a9e;&#x7528;&#x5b78;"/>
+<node CREATED="1609984067081" ID="ID_979139402" LINK="https://zh.wikipedia.org/wiki/Category:&#x5047;&#x8bf4;" MODIFIED="1609984077108" TEXT="Category:&#x5047;&#x8bf4;"/>
+<node CREATED="1609984529453" FOLDED="true" ID="ID_1971792137" LINK="https://ko.wikipedia.org/wiki/&#xc815;&#xce58;" MODIFIED="1609986258317" TEXT="&#xc815;&#xce58; wi">
+<node CREATED="1609985371484" FOLDED="true" ID="ID_1509861278" MODIFIED="1609986258316" TEXT="fur">
+<node CREATED="1609985372933" ID="ID_684365011" LINK="https://en.wikipedia.org/wiki/The_Political_Language_of_Islam" MODIFIED="1609985377419" TEXT="The_Political_Language_of_Islam"/>
+<node CREATED="1609985390565" ID="ID_809353320" LINK="https://en.wikipedia.org/wiki/Politics_and_Language" MODIFIED="1609985394058" TEXT="Politics_and_Language"/>
+</node>
+</node>
+<node CREATED="1609985352294" ID="ID_1230968684" LINK="https://ko.wikipedia.org/wiki/&#xcd1d;&#xc758;" MODIFIED="1609985356378" TEXT="&#xcd1d;&#xc758; wi"/>
+</node>
+</node>
 <node CREATED="1524695902189" ID="ID_1182706123" MODIFIED="1524695910352" POSITION="right" TEXT="1 &#x65e5;&#x672c;&#x8a9e;"/>
+<node CREATED="1609977788789" ID="ID_1794025512" MODIFIED="1609977790848" POSITION="right" TEXT="sessions">
+<node COLOR="#338800" CREATED="1609977791385" FOLDED="true" ID="ID_392185852" MODIFIED="1609986256676" TEXT="s-2">
+<node CREATED="1609977796286" FOLDED="true" ID="ID_1670779246" MODIFIED="1609986256672" TEXT="1">
+<node CREATED="1609977797356" ID="ID_1704779739" MODIFIED="1609977908472" TEXT="read">
+<arrowlink DESTINATION="ID_1618615480" ENDARROW="Default" ENDINCLINATION="165;0;" ID="Arrow_ID_323952835" STARTARROW="None" STARTINCLINATION="165;0;"/>
+</node>
+</node>
+<node CREATED="1609986228053" FOLDED="true" ID="ID_590244195" MODIFIED="1609986256673" TEXT="2">
+<node CREATED="1609986229197" ID="ID_1189369885" MODIFIED="1609986231362" TEXT="search docs"/>
+</node>
+<node CREATED="1609986248285" FOLDED="true" ID="ID_1750136464" MODIFIED="1609986256675" TEXT="n:">
+<node CREATED="1609986251478" ID="ID_960024369" MODIFIED="1609986254362" TEXT="any"/>
+</node>
+<node COLOR="#990000" CREATED="1609986242293" ID="ID_974417552" MODIFIED="1609986244413" TEXT="/20210107_112358"/>
+</node>
+</node>
 <node CREATED="1516766701059" ID="ID_250021227" MODIFIED="1516766703952" POSITION="left" TEXT="2) 5) HISTORY"/>
+<node CREATED="1609453656833" ID="ID_37527138" MODIFIED="1609453658503" POSITION="left" TEXT="utils">
+<node CREATED="1609426935847" ID="ID_677494908" LINK="C:\WORKS_2\t.bat" MODIFIED="1609426941038" TEXT="time"/>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1608627705665" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1608627706940" TEXT="1">
+<node CREATED="1608627706941" ID="ID_1183827369" MODIFIED="1609986304223">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_43.[theme=Linguistics]) periodical updates&quot; &amp;&amp; e &amp;&amp; p &amp;&amp; r oj &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_43.[theme=Linguistics]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_429311715" MODIFIED="1609986318704">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_43.[theme=Linguistics]) periodical updates&quot; &amp;&amp; e &amp;&amp; p &amp;&amp; r oj &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_43.[theme=Linguistics]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
 </node>
 </map>
