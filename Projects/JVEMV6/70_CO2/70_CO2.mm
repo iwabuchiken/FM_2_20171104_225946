684d43d5863bccf4b7850945e40067f834dff551
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-01-22 01:39:35 +0900

    (JVE_70.[theme=CO2]) periodical updates

---
+++ Projects/JVEMV6/70_CO2/70_CO2.mm
@@ -0,0 +1,52 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1611246961910" ID="ID_1657898103" LINK="../../JVEMV6.mm" MODIFIED="1611247009822" TEXT="70. CO2">
+<node CREATED="1611247058158" ID="ID_677845593" MODIFIED="1611247061335" POSITION="right" TEXT="docs">
+<node CREATED="1611247081413" ID="ID_1080327707" LINK="https://ja.wikipedia.org/wiki/&#x8868;&#x9762;&#x5f35;&#x529b;" MODIFIED="1611247086617" TEXT="&#x8868;&#x9762;&#x5f35;&#x529b;"/>
+</node>
+<node CREATED="1611247031122" ID="ID_1857501442" MODIFIED="1611247033666" POSITION="right" TEXT="sessions">
+<node CREATED="1611247039546" ID="ID_575665519" MODIFIED="1611247040672" TEXT="s-1">
+<node COLOR="#990000" CREATED="1611247048116" ID="ID_666472863" MODIFIED="1611247049810" TEXT="20210122_013725/"/>
+<node CREATED="1611247050254" ID="ID_1416741248" MODIFIED="1611247050817" TEXT="1"/>
+<node CREATED="1611247089954" ID="ID_522852302" MODIFIED="1611247091671" TEXT="n:">
+<node CREATED="1611247092644" ID="ID_1645383457" MODIFIED="1611247094783" TEXT="any"/>
+</node>
+</node>
+</node>
+<node CREATED="1609453656833" ID="ID_37527138" MODIFIED="1610686025388" POSITION="left" TEXT="utils">
+<node CREATED="1609426935847" ID="ID_677494908" LINK="C:\WORKS_2\t.bat" MODIFIED="1609426941038" TEXT="time"/>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1610686026668" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1610686027385" TEXT="1">
+<node CREATED="1608627706941" ID="ID_1183827369" LINK="C:\WORKS_2\shortcuts_docs\end_res_physics_[68#theoretical-physics].bat" MODIFIED="1609046346254">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      end apps
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_724708490" MODIFIED="1611247142080">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_70.[theme=CO2]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1610762153011" ID="ID_1685385670" MODIFIED="1610762155265" TEXT="console">
+<node CREATED="1610762155865" ID="ID_1410328441" MODIFIED="1610762157174" TEXT="e3"/>
+</node>
+</node>
+</node>
+</map>
