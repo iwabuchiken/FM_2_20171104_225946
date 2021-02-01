559f11623f6f9eaab81ab99440549ae69e6660f5
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-02-02 01:02:17 +0900

    (JVE_70.[theme=CO2]) periodical updates

--- Projects/JVEMV6/70_CO2/70_CO2.mm
+++ Projects/JVEMV6/70_CO2/70_CO2.mm
@@ -1,6 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1611246961910" ID="ID_1657898103" LINK="../../JVEMV6.mm" MODIFIED="1611247009822" TEXT="70. CO2">
+<node CREATED="1611246961910" ID="ID_1657898103" LINK="../../JVEMV6.mm" MODIFIED="1612194565640" TEXT="70. CO2">
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
 <node CREATED="1611247058158" ID="ID_677845593" MODIFIED="1611247061335" POSITION="right" TEXT="docs">
 <node CREATED="1611247081413" ID="ID_1080327707" LINK="https://ja.wikipedia.org/wiki/&#x8868;&#x9762;&#x5f35;&#x529b;" MODIFIED="1611247086617" TEXT="&#x8868;&#x9762;&#x5f35;&#x529b;"/>
 </node>
@@ -11,6 +12,23 @@
 <node CREATED="1611247089954" ID="ID_522852302" MODIFIED="1611247091671" TEXT="n:">
 <node CREATED="1611247092644" ID="ID_1645383457" MODIFIED="1611247094783" TEXT="any"/>
 </node>
+</node>
+<node COLOR="#338800" CREATED="1612194816838" ID="ID_1707500781" MODIFIED="1612195311315" TEXT="s-2">
+<node CREATED="1612194822166" ID="ID_1924583568" MODIFIED="1612194822166" TEXT="20210202_005333"/>
+<node CREATED="1612194823373" ID="ID_1691273780" MODIFIED="1612194824381" TEXT="1">
+<node CREATED="1612194824381" ID="ID_1590933696" MODIFIED="1612194825707" TEXT="search">
+<node CREATED="1612194828187" ID="ID_700777718" LINK="https://3g2upl4pq6kufc4m.onion/?q=co2+plasma+catalyst&amp;ia=web" MODIFIED="1612194877496" TEXT="co2 plasma catalyst"/>
+<node CREATED="1612195032310" ID="ID_767587385" LINK="https://duckduckgo.com/?q=co2+fixation&amp;t=opera&amp;ia=web&amp;iai=r1-2&amp;page=1&amp;sexp=%7B&quot;artexp&quot;%3A&quot;b&quot;%2C&quot;prodexp&quot;%3A&quot;b&quot;%2C&quot;prdsdexp&quot;%3A&quot;c&quot;%2C&quot;biaexp&quot;%3A&quot;b&quot;%2C&quot;msvrtexp&quot;%3A&quot;b&quot;%7D" MODIFIED="1612195130462" TEXT="co2 fixation"/>
+</node>
+</node>
+<node CREATED="1612195261417" ID="ID_1167464693" MODIFIED="1612195264483" TEXT="n:">
+<node CREATED="1612195264484" ID="ID_312889511" MODIFIED="1612195267658" TEXT="1">
+<node CREATED="1612195267659" ID="ID_1649377993" MODIFIED="1612195283225" TEXT="read">
+<node CREATED="1612195283226" ID="ID_1105125347" LINK="https://www.sciencedirect.com/topics/immunology-and-microbiology/carbon-dioxide-fixation" MODIFIED="1612195296792" TEXT="Carbon Dioxide Fixation"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1612195307587" ID="ID_1674676767" MODIFIED="1612195309780" TEXT="/20210202_010145"/>
 </node>
 </node>
 <node CREATED="1609453656833" ID="ID_37527138" MODIFIED="1610686025388" POSITION="left" TEXT="utils">
@@ -39,8 +57,7 @@
       taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_70.[theme=CO2]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 </node>
 </node>
