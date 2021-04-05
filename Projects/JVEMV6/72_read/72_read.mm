f8c2d185bfb1963b13338f95f5b7588c45dc3367
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-06 00:07:03 +0900

    (JVE_72.[theme=read]) periodical updates

---
+++ Projects/JVEMV6/72_read/72_read.mm
@@ -0,0 +1,91 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1617634486640" ID="ID_56446530" LINK="../../JVEMV6.mm" MODIFIED="1617634527219" TEXT="72. read">
+<node CREATED="1617634543932" ID="ID_1196425684" MODIFIED="1617634550727" POSITION="right" TEXT="9BJ theology">
+<node CREATED="1617634565632" ID="ID_529829575" MODIFIED="1617634568879" TEXT="sessions">
+<node CREATED="1617634575054" ID="ID_748154062" MODIFIED="1617634575054" TEXT="20210405_235612">
+<node CREATED="1617634585585" ID="ID_1615013134" MODIFIED="1617634586819" TEXT="docs">
+<node CREATED="1617634587684" ID="ID_1891708505" LINK="https://ko.wikipedia.org/wiki/YH_&#xc0ac;&#xac74;" MODIFIED="1617634595420" TEXT="YH_&#xc0ac;&#xac74;">
+<node CREATED="1617634597797" ID="ID_774738028" MODIFIED="1617634599016" TEXT="texts"/>
+</node>
+<node CREATED="1617634888500" ID="ID_745113817" LINK="https://news.joins.com/article/1517554" MODIFIED="1617634908737">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#44592;&#46021;&#49324;&#54924;&#51452;&#51032;&#50752; &#54644;&#48169;&#49888;&#54617;|&#51204;&#53685;&#51201;&#51064;
+    </p>
+    <p>
+      &#160;&#54788;&#49892;&#52280;&#50668;&#45716; &#54633;&#48277;&#183;&#54217;&#54868;&#51201;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1617634910730" ID="ID_1104261340" MODIFIED="1617634912028" TEXT="texts">
+<node COLOR="#338800" CREATED="1617634924582" ID="ID_180448012" MODIFIED="1617634946200" TEXT="#1 ::[16:39:51"/>
+<node COLOR="#338800" CREATED="1617634924582" ID="ID_782439861" MODIFIED="1617634946201" TEXT="#2 &#xb178;&#xb3d9;&#xc6b4;&#xb3d9;&#xc758; &#xd6a8;&#xc2dc;&#xac00; &#xb9e8;"/>
+<node COLOR="#338800" CREATED="1617634924582" ID="ID_316492936" MODIFIED="1617635103461" TEXT="#3&#x300c;&#xb3c4;&#xc0b0;&#x300d;&#xc758; &#xd65c;&#xb3d9;&#xc758;">
+<node CREATED="1617634943152" ID="ID_488073939" MODIFIED="1617634943152" TEXT="::[2021&#x5e74;4&#x6708;5&#x65e5;23:42:01"/>
+</node>
+<node CREATED="1617635098712" ID="ID_1134560761" MODIFIED="1617635141492" TEXT="#4 &#xb354;&#xad6c;&#xb098; &#xc885;&#xad50;&#xac00;">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1134560761" ENDARROW="Default" ENDINCLINATION="471;0;" ID="Arrow_ID_845297668" SOURCE="ID_538897312" STARTARROW="None" STARTINCLINATION="471;0;"/>
+<node COLOR="#990000" CREATED="1617635121731" ID="ID_744446080" MODIFIED="1617635123268" TEXT="20210406_000519/"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1617635126897" ID="ID_1003431908" MODIFIED="1617635131328" TEXT="n:">
+<node CREATED="1617635132161" ID="ID_288933968" MODIFIED="1617635134869" TEXT="1">
+<node CREATED="1617635134869" ID="ID_538897312" MODIFIED="1617635141493" TEXT="read">
+<arrowlink DESTINATION="ID_1134560761" ENDARROW="Default" ENDINCLINATION="471;0;" ID="Arrow_ID_845297668" STARTARROW="None" STARTINCLINATION="471;0;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1616304173842" POSITION="left" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1616304175811" TEXT="1">
+<node CREATED="1608627706941" ID="ID_1183827369" LINK="C:\WORKS_2\shortcuts_docs\end_log_JVE_70.[CO2].bat" MODIFIED="1615708628251">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      end apps
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_467605251" LINK="C:\WORKS_2\shortcuts_docs\end_log_JVE_70.[CO2]_apps.bat" MODIFIED="1616661573547">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      end_log_JVE_70.[CO2]_apps
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_724708490" MODIFIED="1617635198833">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_72.[theme=read]) periodical updates&quot; &amp;&amp; e &amp;&amp; p &amp;&amp; r oj &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_72.[theme=read]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
