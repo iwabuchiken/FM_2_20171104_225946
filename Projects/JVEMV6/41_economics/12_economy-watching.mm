4ceebb043011f6300f180c22ec4ac78f42e2c9e0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-04-17 17:46:35 +0900

    (JVE_41.[theme=economics:topic=12_economy-watch]) periodical updates

--- Projects/JVEMV6/41_economics/12_economy-watching.mm
+++ Projects/JVEMV6/41_economics/12_economy-watching.mm
@@ -2,5 +2,131 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1606539205663" ID="ID_1253309608" LINK="../../JVEMV6%2041%20economics.mm" MODIFIED="1606539259781" TEXT="12. economy watching">
 <node CREATED="1606539276261" ID="ID_1344185555" MODIFIED="1606539281145" POSITION="right" TEXT="1. topics"/>
+<node CREATED="1611247031122" ID="ID_1093998936" MODIFIED="1618560637970" POSITION="right" TEXT="sessions">
+<node CREATED="1615187554513" ID="ID_652357099" MODIFIED="1618646796172" TEXT="s-1">
+<node COLOR="#338800" CREATED="1618646797335" FOLDED="true" ID="ID_1626259105" MODIFIED="1618648979367" TEXT="1">
+<node CREATED="1618646807816" ID="ID_697231365" MODIFIED="1618646809634" TEXT="watch">
+<node CREATED="1618646821263" ID="ID_797185808" MODIFIED="1618646822622" TEXT="&#x7d71;&#x8a08;">
+<node CREATED="1618646809635" ID="ID_468626714" MODIFIED="1618646819791" TEXT="&#x30af;&#x30ec;&#x30b8;&#x30c3;&#x30c8;&#x30ab;&#x30fc;&#x30c9;&#x95a2;&#x4fc2;">
+<node CREATED="1618646834710" ID="ID_1761931018" LINK="https://www.j-credit.or.jp" MODIFIED="1618646851936">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#12463;&#12524;&#12472;&#12483;&#12488;&#12459;&#12540;&#12489;&#21332;&#20250;
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1618647048899" ID="ID_1400594453" LINK="https://www.j-credit.or.jp/information/statistics/index.html" MODIFIED="1618647058351" TEXT="&#x30af;&#x30ec;&#x30b8;&#x30c3;&#x30c8;&#x95a2;&#x9023;&#x7d71;&#x8a08;">
+<node CREATED="1618647079719" ID="ID_915842610" MODIFIED="1618647203427" TEXT="s-1.d-1 / &#x4fe1;&#x7528;&#x4f9b;&#x4e0e;&#x984d;&#x30fb;&#x4fe1;&#x7528;&#x4f9b;&#x4e0e;&#x6b8b;&#x9ad8;">
+<node COLOR="#006699" CREATED="1618648057673" ID="ID_799479189" MODIFIED="1618648060054" TEXT="obs">
+<node CREATED="1618648061642" ID="ID_603563329" MODIFIED="1618648157750">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#65298;&#65296;&#65298;&#65296;&#24180;&#12290;&#36820;&#28168;&#32207;&#38989;&#12399;&#12289;&#21069;&#24180;&#12424;&#12426;&#12289;&#32004; 1,700&#20740;&#20870;&#12289;&#22810;&#12356;
+    </p>
+    <p>
+      &#160;(==&gt; up 1.95%)
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1618648195246" ID="ID_924926244" MODIFIED="1618648258942">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2018 yr
+    </p>
+    <p>
+      &#20379;&#19982;&#22679;&#28187;&#12289;&#21516;&#27604; 8.2 %
+    </p>
+    <p>
+      &#36820;&#28168;&#22679;&#28187;&#12289;&#21516;&#27604; 4.76 %
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1618648336977" ID="ID_502684995" MODIFIED="1618648339915" TEXT="sheet">
+<node CREATED="1618648341458" ID="ID_236734638" LINK="C:\WORKS_2\WS\WS_Others.JVEMV6\JVEMV6\41_economics\12_economy-watching\s-1.(1).ods" MODIFIED="1618648369436" TEXT="s-1.(1).ods / 1.(2)"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1610723630275" ID="ID_1528978459" MODIFIED="1615959628072" TEXT="n:">
+<node CREATED="1610781936849" ID="ID_132021666" MODIFIED="1610781937241" TEXT="1">
+<node CREATED="1618648818046" ID="ID_408751464" LINK="https://www.j-credit.or.jp/information/statistics/index.html#shopping-movement" MODIFIED="1618648909011" TEXT="&#x4eca;&#x56de;&#x3068;&#x540c;&#x3058;&#x30b5;&#x30a4;&#x30c8;&#x3002;&#x6708;&#x6b21;&#x7d71;&#x8a08;"/>
+</node>
+<node CREATED="1618648912846" ID="ID_1193324839" MODIFIED="1618648915311" TEXT="2">
+<node CREATED="1618648915313" ID="ID_1806303152" MODIFIED="1618648922149" TEXT="&#x30b9;&#x30fc;&#x30d1;&#x30fc;&#x95a2;&#x9023;&#x306e;&#x7d71;&#x8a08;"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1618648964957" ID="ID_1156432193" MODIFIED="1618648967091" TEXT="/20210417_174243"/>
+</node>
+</node>
+<node CREATED="1609453656833" ID="ID_37527138" MODIFIED="1610686025388" POSITION="left" TEXT="utils">
+<node CREATED="1609426935847" ID="ID_677494908" LINK="C:\WORKS_2\t.bat" MODIFIED="1609426941038" TEXT="time"/>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1610686026668" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1610686027385" TEXT="1">
+<node CREATED="1608627706941" ID="ID_607619239" LINK="C:\WORKS_2\shortcuts_docs\end_log_JVE_41.[economics_12_watching].bat" MODIFIED="1618649177744">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      end apps
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_724708490" MODIFIED="1618649142224">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      taskkill /f /im javaw.exe &amp;&amp; taskkill /f /im sakura.exe &amp;&amp; C:\WORKS_2\a &amp;&amp; r f &amp;&amp; g a . &amp;&amp; g c -m &quot;(JVE_41.[theme=economics:topic=12_economy-watch]) periodical updates&quot; &amp;&amp; e &amp;&amp; p
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1610762153011" ID="ID_1685385670" MODIFIED="1610762155265" TEXT="console">
+<node CREATED="1610762155865" ID="ID_1410328441" MODIFIED="1610762157174" TEXT="e3"/>
+</node>
+<node CREATED="1617515827355" ID="ID_261421940" MODIFIED="1617515829814" TEXT="steps">
+<node CREATED="1617515829815" ID="ID_1123020409" MODIFIED="1617515831562" TEXT="closing">
+<node CREATED="1617515832426" ID="ID_94076224" MODIFIED="1617515838137" TEXT="add timestamp"/>
+<node CREATED="1617515839235" ID="ID_633166525" MODIFIED="1617515846304" TEXT="session label ==&gt; green"/>
+<node CREATED="1617515847604" ID="ID_1918558258" MODIFIED="1617515852380" TEXT="C/P --&gt; next session"/>
+<node CREATED="1617515853868" ID="ID_970861100" MODIFIED="1617515854743" TEXT="git"/>
+</node>
+</node>
+</node>
+<node CREATED="1615187554513" ID="ID_699627215" MODIFIED="1617690330048" POSITION="right" TEXT="s-X">
+<node CREATED="1610723630275" ID="ID_1040116369" MODIFIED="1615959628072" TEXT="n:">
+<node CREATED="1610781936849" ID="ID_801534641" MODIFIED="1610781937241" TEXT="1"/>
+</node>
+</node>
 </node>
 </map>
