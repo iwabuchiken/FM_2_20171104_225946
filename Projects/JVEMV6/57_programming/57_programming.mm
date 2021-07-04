99117f6f04f114db17fa9cc457d53d31b6bb8b4f
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-07-04 17:58:58 +0900

    (JVE_57.[programming]) periodical updates

---
+++ Projects/JVEMV6/57_programming/57_programming.mm
@@ -0,0 +1,80 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1549835897611" ID="ID_958443378" LINK="../../JVEMV6.mm" MODIFIED="1625388753578" TEXT="57 programming">
+<node CREATED="1625388784434" ID="ID_704448431" MODIFIED="1625388786625" POSITION="right" TEXT="genres">
+<node CREATED="1549835921039" ID="ID_33715715" MODIFIED="1625388787494" TEXT="1 get_folder_list"/>
+<node CREATED="1625388797922" ID="ID_7438156" MODIFIED="1625388801521" TEXT="2">
+<node CREATED="1625388801521" ID="ID_1008401378" MODIFIED="1625388806860" TEXT="physics engine"/>
+<node CREATED="1625388827085" ID="ID_1566095520" LINK="C:\WORKS_2\shortcuts_docs\log_[20210629_230813].[physics-engine-python].txt" MODIFIED="1625388835824" TEXT="log file"/>
+</node>
+</node>
+<node CREATED="1608648934595" ID="ID_349866319" MODIFIED="1625388773237" POSITION="right" TEXT="sessions">
+<node COLOR="#338800" CREATED="1615187554513" ID="ID_1265025589" MODIFIED="1625389007261" TEXT="s-2 / g-2">
+<node CREATED="1610723630275" ID="ID_1174493643" MODIFIED="1615959628072" TEXT="n:">
+<node CREATED="1610781936849" ID="ID_1726855265" MODIFIED="1610781937241" TEXT="1"/>
+<node CREATED="1624859724607" ID="ID_1350079268" MODIFIED="1624859725347" TEXT="2"/>
+</node>
+</node>
+<node CREATED="1609426935847" ID="ID_103785999" LINK="C:\WORKS_2\t.bat" MODIFIED="1625362355770" TEXT="time"/>
+</node>
+<node CREATED="1609453656833" ID="ID_37527138" MODIFIED="1610686025388" POSITION="left" TEXT="utils">
+<node CREATED="1609426935847" ID="ID_677494908" LINK="C:\WORKS_2\t.bat" MODIFIED="1609426941038" TEXT="time"/>
+<node CREATED="1608627704595" ID="ID_1116133850" MODIFIED="1610686026668" TEXT="git">
+<node CREATED="1608627706371" ID="ID_492644994" MODIFIED="1610686027385" TEXT="1">
+<node CREATED="1618886168751" ID="ID_1641120082" MODIFIED="1625389021343">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      set stringof_session_id=&quot;s-43 / g-23s-2 / g-2&quot;
+    </p>
+    <p>
+      set stringof_project_id=&quot;JVE_57.[programming] :&#160;%stringof_session_id:&quot;=%&quot;
+    </p>
+    <p>
+      C:\WORKS_2\shortcuts_docs\end_log_JVE_ALL.[ai].git.bat %stringof_project_id:&quot;=%
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_379061315" LINK="C:\WORKS_2\shortcuts_docs\end_log_JVE_68.[tp_v-2.0].[git].bat" MODIFIED="1623031309491">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      end : git
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1608627706941" ID="ID_1183827369" LINK="C:\WORKS_2\shortcuts_docs\end_log_JVE_68.[tp_v-2.0].bat" MODIFIED="1622716087259">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      end apps
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1610762153011" ID="ID_1685385670" MODIFIED="1610762155265" TEXT="console">
+<node CREATED="1610762155865" ID="ID_1410328441" MODIFIED="1610762157174" TEXT="e3"/>
+</node>
+</node>
+<node CREATED="1615187554513" ID="ID_699627215" MODIFIED="1617690330048" POSITION="right" TEXT="s-X">
+<node CREATED="1610723630275" ID="ID_1040116369" MODIFIED="1615959628072" TEXT="n:">
+<node CREATED="1610781936849" ID="ID_801534641" MODIFIED="1610781937241" TEXT="1"/>
+<node CREATED="1624859724607" ID="ID_502901422" MODIFIED="1624859725347" TEXT="2"/>
+</node>
+</node>
+</node>
+</map>
