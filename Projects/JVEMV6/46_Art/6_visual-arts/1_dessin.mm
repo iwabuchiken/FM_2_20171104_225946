8a37d7c7d20cde448a4262151f94f77b5a026cb0
iwabuchiken <iwabuchi.k.2010@gmail.com>
2022-03-09 16:23:42 +0900

    (JVE_68.[tp_v-2.0]) periodical updates

---
+++ Projects/JVEMV6/46_Art/6_visual-arts/1_dessin.mm
@@ -0,0 +1,104 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1522305238623" ID="ID_1907301908" LINK="../../../46%20Art.6%20Visual%20Arts.mm" MODIFIED="1607875371457">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1 dessin
+    </p>
+    <p>
+      (==&gt;sketch)
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1607872670871" ID="ID_943645078" MODIFIED="1607872676409" POSITION="right" TEXT="1. &#x4eba;">
+<node CREATED="1597910503348" ID="ID_1669128646" MODIFIED="1607872665200" TEXT="1. &#x3057;&#x3083;&#x304c;&#x3080;">
+<node CREATED="1597910507972" ID="ID_786734302" LINK="https://stat.ameba.jp/user_images/20121022/23/aeolia/62/ca/j/t02200331_0800120512250325479.jpg" MODIFIED="1597910520657" TEXT="1">
+<node CREATED="1597910569891" ID="ID_151452092" LINK="https://ameblo.jp/aeolia/entry-11386365049.html" MODIFIED="1597910572451" TEXT="source"/>
+</node>
+</node>
+<node CREATED="1607872692610" ID="ID_668937421" MODIFIED="1607872701284" TEXT="2.  &#x624b;">
+<node CREATED="1607872702118" ID="ID_1615618658" MODIFIED="1607872708250" TEXT="1. &#x63b4;&#x3080;">
+<node CREATED="1607872709114" ID="ID_500110700" MODIFIED="1607872720230" TEXT="1. &#x7bb8;">
+<node CREATED="1607873135069" ID="ID_322268884" MODIFIED="1607873136880" TEXT="search">
+<node CREATED="1607873138013" ID="ID_327697949" LINK="https://duckduckgo.com/?q=&#x624b;+&#x7bb8;&amp;iar=images&amp;iax=images&amp;ia=images" MODIFIED="1607873149613" TEXT="1"/>
+</node>
+<node CREATED="1607873150929" ID="ID_1031478708" MODIFIED="1607873154690" TEXT="docs">
+<node CREATED="1607873159548" ID="ID_1110600283" LINK="https://www.marukome.co.jp/marukome_omiso/hakkoubishoku/20140808/4068/" MODIFIED="1607873162272" TEXT="1">
+<node CREATED="1607873170563" ID="ID_321244304" LINK="https://www.marukome.co.jp/marukome_omiso/hakkoubishoku/uploads/2014/07/wa1_0730.jpg" MODIFIED="1607873823150" TEXT="image">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_321244304" ENDARROW="Default" ENDINCLINATION="89;0;" ID="Arrow_ID_661022357" SOURCE="ID_1453228779" STARTARROW="None" STARTINCLINATION="89;0;"/>
+</node>
+<node CREATED="1607873470358" ID="ID_1336901235" LINK="http://benfranklin.chips.jp/WS/WS_Others/free/UH8G6E_CE/1_X/1_14/main.(1.14~).php?url=https://www.marukome.co.jp/marukome_omiso/hakkoubishoku/uploads/2014/07/wa1_0730.jpg" MODIFIED="1607873476857" TEXT="util"/>
+<node CREATED="1607997398681" ID="ID_1478595697" MODIFIED="1607997401092" TEXT="words">
+<node CREATED="1607997401093" ID="ID_878553068" MODIFIED="1607997401093" TEXT="">
+<node CREATED="1607997401241" ID="ID_363437692" MODIFIED="1607997408526" TEXT="&#x5343;&#x4ee3;&#x7d50;&#x3073;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1607873674996" ID="ID_714587646" MODIFIED="1607873677563" POSITION="right" TEXT="sessions">
+<node CREATED="1607873677564" ID="ID_686439279" MODIFIED="1607873678766" TEXT="s-1">
+<node CREATED="1607873686948" ID="ID_1277691828" MODIFIED="1607873686948" TEXT="20201214_003445">
+<node CREATED="1607873815894" ID="ID_107146980" MODIFIED="1607873818696" TEXT="doc">
+<node CREATED="1607873818697" ID="ID_1453228779" MODIFIED="1607873823150" TEXT="1.">
+<arrowlink DESTINATION="ID_321244304" ENDARROW="Default" ENDINCLINATION="89;0;" ID="Arrow_ID_661022357" STARTARROW="None" STARTINCLINATION="89;0;"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1607957728657" ID="ID_1009346396" MODIFIED="1607957730213" TEXT="s-2">
+<node CREATED="1607957771134" ID="ID_847715447" MODIFIED="1607957771134" TEXT="20201214_235535"/>
+<node CREATED="1607957938835" ID="ID_9064250" MODIFIED="1607957942892" TEXT="&#x7bb8;">
+<node CREATED="1607957944381" ID="ID_831083374" MODIFIED="1607957945489" TEXT="docs">
+<node CREATED="1607957947180" ID="ID_113281599" LINK="https://rinrinto.com/hanataka-20191205-1-3348" MODIFIED="1607957950941" TEXT="1">
+<node CREATED="1607957985867" ID="ID_1572183432" MODIFIED="1607957995275" TEXT="==&gt; image can&apos;t be opened"/>
+</node>
+<node CREATED="1607957996437" ID="ID_547665696" LINK="https://youjiokeiko.gakken.jp/idea/ikuji-mini/3316" MODIFIED="1607957997646" TEXT="2">
+<node CREATED="1607958001956" ID="ID_1556758968" LINK="https://youjiokeiko.gakken.jp/wps/wp-content/uploads/2018/10/hasi001.jpg" MODIFIED="1607958004102" TEXT="image"/>
+<node CREATED="1607958035392" ID="ID_1015591758" LINK="http://benfranklin.chips.jp/WS/WS_Others/free/UH8G6E_CE/1_X/1_14/main.(1.14~).php?url=https://youjiokeiko.gakken.jp/wps/wp-content/uploads/2018/10/hasi001.jpg" MODIFIED="1607958041564" TEXT="util"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1607958213004" ID="ID_570393879" MODIFIED="1607958221274" POSITION="right" TEXT="resources">
+<node CREATED="1607958222107" ID="ID_262115596" MODIFIED="1607958225119" TEXT="1. &#x624b;">
+<node CREATED="1607958245249" ID="ID_1539176813" MODIFIED="1607958256344" TEXT="search">
+<node CREATED="1607958248331" ID="ID_7712492" LINK="https://duckduckgo.com/?q=&#x624b;+&#x9aa8;&#x683c;&amp;t=opera&amp;ia=web" MODIFIED="1607958250305" TEXT="1"/>
+</node>
+<node CREATED="1607958253520" ID="ID_68436099" MODIFIED="1607958254472" TEXT="docs">
+<node CREATED="1607958226373" ID="ID_1986435634" LINK="https://kokansetsu-itami.com/bonejflig/7468/" MODIFIED="1607958483014" TEXT="1. &#x624b;&#x306e;&#x9aa8;&#x306e;&#x89e3;&#x5256;&#x3092;&#x540d;&#x524d;&#x3068;&#x90e8;&#x4f4d;&#x304c;&#x8ab0;&#x3067;&#x3082;&#x308f;&#x304b;&#x308b;&#x3088;&#x3046;&#x306b;&#x56f3;&#x3067;&#x89e3;&#x8aac;&#x3057;&#x3066;&#x307f;&#x305f;"/>
+<node CREATED="1607958457326" ID="ID_1063528115" LINK="https://medical-symptoms.net/archives/4977" MODIFIED="1607958466048" TEXT="2. &#x3010;&#x56f3;&#x89e3;&#x3011;&#x624b;&#x306e;&#x6307;&#x306e;&#x9aa8;&#x306e;&#x540d;&#x524d;&#x30fb;&#x95a2;&#x7bc0;&#x307e;&#x3068;&#x3081;&#xff01;&#x75db;&#x307f;&#x3084;&#x5909;&#x5f62;&#x304c;&#x3042;&#x308b;&#x3068;&#x3069;&#x3093;&#x306a;&#x53ef;&#x80fd;&#x6027;&#x304c;&#xff1f;"/>
+</node>
+<node CREATED="1607997325481" ID="ID_588079367" MODIFIED="1607997331526" TEXT="&#x6458;&#x3080;">
+<node CREATED="1607997333702" ID="ID_385870083" LINK="https://www.cedyna-mail.jp/mail/cn/kenbunroku/index_190319.html" MODIFIED="1607997359019" TEXT="1">
+<node CREATED="1607997345698" ID="ID_1372766798" LINK="https://3g2upl4pq6kufc4m.onion/?q=&#x5343;&#x4ee3;&#x7d50;&#x3073;&amp;iar=images&amp;iax=images&amp;ia=images" MODIFIED="1607997351552" TEXT="srch"/>
+</node>
+</node>
+</node>
+<node CREATED="1607997451869" ID="ID_1677002889" MODIFIED="1607997456878" TEXT="2. &#x5efa;&#x7bc9;">
+<node CREATED="1607997457804" ID="ID_528423463" MODIFIED="1607997462564" TEXT="1. &#x6a4b;">
+<node CREATED="1607997464084" ID="ID_1356599139" MODIFIED="1607997469405" TEXT="&#x773c;&#x93e1;&#x6a4b;">
+<node CREATED="1607997474733" ID="ID_1140684394" LINK="https://www.cedyna-mail.jp/mail/cn/kenbunroku/index_190319.html" MODIFIED="1607997477441" TEXT="1"/>
+</node>
+</node>
+</node>
+<node CREATED="1607997491709" ID="ID_1047586507" MODIFIED="1607997494596" TEXT="3. &#x6599;&#x7406;">
+<node CREATED="1607997496427" ID="ID_1520574157" MODIFIED="1607997505383" TEXT="1. &#x5bff;&#x53f8;">
+<node CREATED="1607997508650" ID="ID_746385650" LINK="https://www.cedyna-mail.jp/mail/cn/kenbunroku/index_190319.html" MODIFIED="1607997523518" TEXT="1. &#x3061;&#x3089;&#x3057;&#x5bff;&#x53f8;"/>
+</node>
+</node>
+</node>
+<node CREATED="1607873346514" ID="ID_58158943" MODIFIED="1607873349043" POSITION="left" TEXT="utils">
+<node CREATED="1607873350016" ID="ID_840199970" LINK="http://benfranklin.chips.jp/WS/WS_Others/free/UH8G6E_CE/1_X/1_14/main.(1.14~).php" MODIFIED="1607873417048" TEXT="remote server"/>
+<node CREATED="1607960133243" ID="ID_1846414920" LINK="C:\WORKS_2\Utils\commands\push_fm.bat" MODIFIED="1607960150084" TEXT="git"/>
+</node>
+</node>
+</map>
